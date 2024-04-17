7314ff616022d2334c698556bac526c568530569
Fabian Zierler <fabian.zierler@a1.net>
2024-04-17 19:52:38 +0100

    split label into channel and overall momentum

--- scripts/test_parse.jl
+++ scripts/test_parse.jl
@@ -13,6 +13,20 @@ function _write_lattice_setup(file,h5file;h5group="")
     h5write(h5file,joinpath(h5group,"lattice"),latticesize(file))
     h5write(h5file,joinpath(h5group,"quarkmasses"),fermionmasses(file))
 end
+function _splitlabel(label)
+    @show label, contains(label,"p(") 
+    label == "pi" && (return label, "p(0,0,0)")
+    if contains(label,"p0") 
+        l = replace(label,"_p0"=>"")
+        return l, "p(0,0,0)"
+    elseif contains(label,"p(")
+        f = first(findfirst("_p(",label))
+        n = findlast(')',label)
+        l = label[1:f-1]*label[n+1:end]
+        p = label[f+1:n]
+        return l, p
+    end
+end
 
 # HDF5 Struktur wie mit Yannick besprochen
 """" 
