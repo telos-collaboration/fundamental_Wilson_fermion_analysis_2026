7d6a34f65523fe10c56aa8929eae6de455c8662f
Fabian Zierler <fabian.zierler@a1.net>
2024-06-07 15:54:28 +0100

    overview plot for p=(0,0,1)

--- scripts/plot_diagrams.jl
+++ scripts/plot_diagrams.jl
@@ -16,14 +16,43 @@ end
 hdf5file = "isospin1_raw.hdf5"
 h5dset = h5open(hdf5file)
 
-Corr = h5dset["E1/p(0,0,1)/r1/p_diag(0,0,1)/C_re"][]
-Corr = h5dset["E1/p(0,0,1)/d/p_diag(0,0,1)/C_re"][]
-Corr = h5dset["E1/p(0,0,1)/pi/p_diag(0,0,1)/C_re"][]
-Corr = h5dset["E1/p(0,0,1)/rho_g33/p_diag(0,0,1)/C_re"][]
-Corr = h5dset["E1/p(1,1,0)/t1_g3/p_diag(1,1,0)/C_im"][]
+CorrD  = h5dset["E1/p(0,0,1)/d/p_diag(0,0,1)/C_re"][]
+Corrπ  = h5dset["E1/p(0,0,1)/pi/p_diag(0,0,1)/C_re"][]
+Corrρ  = h5dset["E1/p(0,0,1)/rho_g33/p_diag(0,0,1)/C_re"][]
+CorrT1 = -h5dset["E1/p(0,0,1)/t1_g3/p_diag(0,0,1)/C_im"][]
+CorrT2 =  h5dset["E1/p(0,0,1)/t2_g3/p_diag(0,0,1)/C_im"][]
 
-C_re, ΔC_re = _average_correlator(Corr)
+CorrR1 = h5dset["E1/p(0,0,1)/r1/p_diag(0,0,1)/C_re"][]
+CorrR2 = h5dset["E1/p(0,0,1)/r2/p_diag(0,0,1)/C_re"][]
+CorrR3 = h5dset["E1/p(0,0,1)/r3/p_diag(0,0,1)/C_re"][]
+CorrR4 = h5dset["E1/p(0,0,1)/r4/p_diag(0,0,1)/C_re"][]
 
-plt = plot()
-scatter!(plt,C_re,yerr=ΔC_re,yscale=:log10)
+CD,  ΔCD  = _average_correlator(CorrD)
+Cπ,  ΔCπ  = _average_correlator(Corrπ)
+Cρ,  ΔCρ  = _average_correlator(Corrρ)
+CT1, ΔCT1 = _average_correlator(CorrT1)
+CT2, ΔCT2 = _average_correlator(CorrT2)
 
+CR1, ΔCR1 = _average_correlator(CorrR1)
+CR2, ΔCR2 = _average_correlator(CorrR2)
+CR3, ΔCR3 = _average_correlator(CorrR3)
+CR4, ΔCR4 = _average_correlator(CorrR4)
+
+pltPiPi = plot()
+pltMes  = plot()
+pltCross = plot()
+scatter!(pltMes,Cπ, yerr=ΔCπ,yscale=:log10, label="(π)")
+scatter!(pltMes,Cρ, yerr=ΔCρ,yscale=:log10, label="(ρ)")
+
+scatter!(pltCross,CT1,yerr=ΔCT1,yscale=:log10,label="(-Im(T1))")
+scatter!(pltCross,CT2,yerr=ΔCT2,yscale=:log10,label="(+Im(T2))")
+
+scatter!(pltPiPi,CD, yerr=ΔCD,yscale=:log10,label="(D)")
+scatter!(pltPiPi,CR1,yerr=ΔCR1,yscale=:log10,label="(R1)")
+scatter!(pltPiPi,CR2,yerr=ΔCR2,yscale=:log10,label="(R2)")
+scatter!(pltPiPi,CR3,yerr=ΔCR3,yscale=:log10,label="(R3)")
+scatter!(pltPiPi,CR4,yerr=ΔCR4,yscale=:log10,label="(R4)")
+
+pltCross
+pltMes
+pltPiPi
