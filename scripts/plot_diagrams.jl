27be452a74e287c4365a2789833d9484cee21991
Fabian Zierler <fabian.zierler@a1.net>
2024-06-10 15:33:18 +0100

    more plots, more stochastic sources

--- scripts/plot_diagrams.jl
+++ scripts/plot_diagrams.jl
@@ -13,7 +13,8 @@ function _average_correlator(Corr)
 end
 
 # Lt32Ls24beta6.9m1-0.92m2-0.92
-hdf5file = "isospin1_raw.hdf5"
+
+hdf5file = "isospin1_h16_raw.hdf5"
 h5dset = h5open(hdf5file)
 
 CorrD  = h5dset["E1/p(0,0,1)/d/p_diag(0,0,1)/C_re"][]
@@ -24,8 +25,10 @@ CorrT2 =  h5dset["E1/p(0,0,1)/t2_g3/p_diag(0,0,1)/C_im"][]
 
 CorrR1 = h5dset["E1/p(0,0,1)/r1/p_diag(0,0,1)/C_re"][]
 CorrR2 = h5dset["E1/p(0,0,1)/r2/p_diag(0,0,1)/C_re"][]
-CorrR3 = h5dset["E1/p(0,0,1)/r3/p_diag(0,0,1)/C_re"][]
-CorrR4 = h5dset["E1/p(0,0,1)/r4/p_diag(0,0,1)/C_re"][]
+CorrR3 = h5dset["E1/p(0,0,1)/r3/p_diag(0,0,1)/C_im"][]
+CorrR4 = h5dset["E1/p(0,0,1)/r4/p_diag(0,0,1)/C_im"][]
+
+N, nhits, T = size(CorrD)
 
 CD,  ΔCD  = _average_correlator(CorrD)
 Cπ,  ΔCπ  = _average_correlator(Corrπ)
@@ -41,18 +44,21 @@ CR4, ΔCR4 = _average_correlator(CorrR4)
 pltPiPi = plot()
 pltMes  = plot()
 pltCross = plot()
-scatter!(pltMes,Cπ, yerr=ΔCπ,yscale=:log10, label="(π)")
-scatter!(pltMes,Cρ, yerr=ΔCρ,yscale=:log10, label="(ρ)")
 
-scatter!(pltCross,CT1,yerr=ΔCT1,yscale=:log10,label="(-Im(T1))")
-scatter!(pltCross,CT2,yerr=ΔCT2,yscale=:log10,label="(+Im(T2))")
+scatter!(pltMes,Cπ, yerr=ΔCπ,yscale=:log10, label="(π) n_src = $nhits")
+scatter!(pltMes,Cρ, yerr=ΔCρ,yscale=:log10, label="(ρ) n_src = $nhits")
 
-scatter!(pltPiPi,CD, yerr=ΔCD,yscale=:log10,label="(D)")
-scatter!(pltPiPi,CR1,yerr=ΔCR1,yscale=:log10,label="(R1)")
-scatter!(pltPiPi,CR2,yerr=ΔCR2,yscale=:log10,label="(R2)")
-scatter!(pltPiPi,CR3,yerr=ΔCR3,yscale=:log10,label="(R3)")
-scatter!(pltPiPi,CR4,yerr=ΔCR4,yscale=:log10,label="(R4)")
+scatter!(pltCross,CT1,yerr=ΔCT1,yscale=:log10,label="-Im(T1) n_src = $nhits")
+scatter!(pltCross,CT2,yerr=ΔCT2,yscale=:log10,label="+Im(T2) n_src = $nhits")
 
-pltCross
+scatter!(pltPiPi,CD, yerr=ΔCD ,yscale=:log10,label="(D) n_src = $nhits")
+# These correlators dip below zero: Study nhit-dependence
+# Also, study autocorrelation, topological freezing and related
+#scatter!(pltPiPi,CR1,yerr=ΔCR1,yscale=:log10,label="(R1) n_src = $nhits")
+#scatter!(pltPiPi,CR2,yerr=ΔCR2,yscale=:log10,label="(R2) n_src = $nhits")
+#scatter!(pltPiPi,CR3,yerr=ΔCR3,label="(R3) n_src = $nhits")
+#scatter!(pltPiPi,CR4,yerr=ΔCR4,label="(R4) n_src = $nhits")
+
 pltMes
+pltCross
 pltPiPi
--- scripts/test_parse.jl
+++ scripts/test_parse.jl
@@ -1,6 +1,10 @@
 using Pkg; Pkg.activate(".")
 using ScatteringI1
 
-file ="/home/fabian/Downloads/out_scattering_I1"
-h5file = "isospin1_raw.hdf5"
+#file ="/home/fabian/Downloads/out_scattering_I1"
+#h5file = "isospin1_h4_raw.hdf5"
+#isospin1_to_hdf5(file,h5file,ensemble="E1")
+
+file ="/home/fabian/Downloads/out_scattering_I1_h16"
+h5file = "isospin1_h16_raw.hdf5"
 isospin1_to_hdf5(file,h5file,ensemble="E1")
