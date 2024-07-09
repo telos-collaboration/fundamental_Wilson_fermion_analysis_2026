e3ff79829f0abd279962b37433ad45275805b050
Fabian Zierler <fabian.zierler@a1.net>
2024-07-09 16:32:44 +0100

    compare zero-momentum volumes

--- scripts/plot_diagrams_volume.jl
+++ scripts/plot_diagrams_volume.jl
@@ -4,8 +4,8 @@ using Plots
 using HDF5
 using Statistics
 using LaTeXStrings
-plotlyjs(frame=:box, legend=:topright,legendfontsize=12)
 pgfplotsx(frame=:box, legend=:topright,labelfontsize=16, titlefontsize=16, legendfontsize=14,markersize=5,tickfontsize=12)
+plotlyjs(frame=:box, legend=:topright,legendfontsize=12)
 include("utils.jl")
 
 # Lt32Ls24beta6.9m1-0.92m2-0.92
@@ -23,8 +23,8 @@ ret2 = read_hdf5_file(file2,ens,p1)
 
 L1 = ret1[2]
 L2 = ret2[2]
-Corr1 = ret1[3] 
-Corr2 = ret2[3] 
+Corr1 = ret1[11] 
+Corr2 = ret2[11] 
 
 title = L"\beta=6.9, m_0^f=-0.92, \mathbf p = %$p1"
 C1,  ΔC1  = _average_correlator(Corr1)
@@ -33,9 +33,10 @@ C2,  ΔC2  = _average_correlator(Corr2)
 plt1 = plot(; title, xlabel=L"t",ylabel=L"C(t)",yscale=:log10,legend=:top)
 plt2 = plot(; title, xlabel=L"t",ylabel=L"$C(t)$ ratio",legend=:top)
 
-scatter!(plt1,C1, yerr=ΔC1, label=L"\pi (L=%$L1)" ,marker=:rect,alpha=0.9)
-scatter!(plt1,C2, yerr=ΔC2, label=L"\pi (L=%$L2)" ,marker=:rect,alpha=0.9)
+scatter!(plt1,C1 ./ L1^6, yerr=ΔC1 ./ L1^6, label=L"\pi (L=%$L1)" ,marker=:rect,alpha=0.9)
+scatter!(plt1,C2 ./ L2^6, yerr=ΔC2 ./ L2^6, label=L"\pi (L=%$L2)" ,marker=:rect,alpha=0.9)
 
 scatter!(plt2,C1 ./ C2, yerr=ΔC1 ./ C2 .+ ΔC2 .* C1 ./ C2 .^ 2 , label="" ,marker=:rect,alpha=0.9)
 
 display(plt2)
+display(plt1)
--- scripts/utils.jl
+++ scripts/utils.jl
@@ -73,18 +73,17 @@ function correlatorsp110(h5dset,ens)
     return Corrπ, Corrρ, CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2
 end
 function correlatorsp000(h5dset,ens)
-    p1 = "(0,0,0)"
-    CorrD1 = D1(h5dset,p1)
-    CorrD2 = h5dset["$ens/p$p1/d/p_diag$p1/C_re"][]
-    Corrπ  = h5dset["$ens/p$p1/pi/p_diag$p1/C_re"][]
-    # THE FOLLOWING DOES NOT GIVE A SIGNAL. IS SOMETHING WRONG WITH THE PARSING?
-    Corrρ  =  (h5dset["$ens/p$p1/rho_g11/p_diag$p1/C_re"][])
-    #Corrρ  =  (h5dset["$ens/p$p1/rho_g22/p_diag$p1/C_re"][])
-    CorrT1 = -(h5dset["$ens/p$p1/t1_g1/p_diag$p1/C_im"][]   + h5dset["E1/p$p1/t1_g2/p_diag$p1/C_im"][])/sqrt(2)
-    CorrT2 =  (h5dset["$ens/p$p1/t2_g1/p_diag$p1/C_im"][]   + h5dset["E1/p$p1/t2_g2/p_diag$p1/C_im"][])/sqrt(2)
-    CorrR1 =   h5dset["$ens/p$p1/r1/p_diag$p1/C_re"][]
-    CorrR2 =   h5dset["$ens/p$p1/r2/p_diag$p1/C_re"][]
-    CorrR3 =   h5dset["$ens/p$p1/r3/p_diag$p1/C_re"][]
+    CorrD1 = D1(h5dset,"(0,0,0)")
+    CorrD2 = h5dset["$ens/p(0,0,1)/d/p_diag(0,0,0)/C_re"][]
+    Corrπ  = h5dset["$ens/p(0,0,0)/pi/p_diag(0,0,0)/C_re"][]
+    # THE FOLLOWING IS NOT FULLY SELFCONSISTENT
+    # It is used to compare the correlator normalization
+    Corrρ  =  h5dset["$ens/p(0,0,0)/rho_g1/p_diag(0,0,0)/C_re"][]
+    CorrT1 = -h5dset["$ens/p(0,0,1)/t1_g1/p_diag(0,0,0)/C_im"][]  
+    CorrT2 =  h5dset["$ens/p(0,0,1)/t2_g1/p_diag(0,0,0)/C_im"][] 
+    CorrR1 =  h5dset["$ens/p(0,0,1)/r1/p_diag(0,0,0)/C_re"][]
+    CorrR2 =  h5dset["$ens/p(0,0,1)/r2/p_diag(0,0,0)/C_re"][]
+    CorrR3 =  h5dset["$ens/p(0,0,1)/r3/p_diag(0,0,0)/C_re"][]
     return Corrπ, Corrρ, CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2
 end
 function read_hdf5_file(file,ens,p1)
