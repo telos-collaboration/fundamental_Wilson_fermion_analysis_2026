6c6bed9dfd6818880b3368240f508fba36988077
Fabian Zierler <fabian.zierler@a1.net>
2024-07-12 13:28:20 +0100

    compare all diagrams, confirm normalisation

--- scripts/plot_diagrams_volume.jl
+++ scripts/plot_diagrams_volume.jl
@@ -5,7 +5,7 @@ using HDF5
 using Statistics
 using LaTeXStrings
 pgfplotsx(frame=:box, legend=:topright,labelfontsize=16, titlefontsize=16, legendfontsize=14,markersize=5,tickfontsize=12)
-plotlyjs(frame=:box, legend=:topright,legendfontsize=12)
+gr(frame=:box, legend=:topright,legendfontsize=12)
 include("utils.jl")
 
 # Lt32Ls24beta6.9m1-0.92m2-0.92
@@ -23,25 +23,27 @@ ret2 = read_hdf5_file(file2,ens,p1B,3)
 
 L1 = ret1[2]
 L2 = ret2[2]
-Corr1 = ret1[11] 
-Corr2 = ret2[11] 
+ind_corr = 12
+Corr1 = ret1[ind_corr] 
+Corr2 = ret2[ind_corr] 
 
-title = L"\beta=6.9, m_0^f=-0.92, \mathbf p = %$p1"
 C1,  ΔC1  = _average_correlator(Corr1)
 C2,  ΔC2  = _average_correlator(Corr2)
 
-plt1 = plot(; title, xlabel=L"t",ylabel=L"C(t)",yscale=:log10,legend=:top)
-plt2 = plot(; title, xlabel=L"t",ylabel=L"$C(t)$ ratio",legend=:top)
+plt1 = plot(;xlabel=L"t",ylabel=L"C(t)",yscale=:log10,legend=:top)
+plt2 = plot(;xlabel=L"t",ylabel=L"$C(t)$ ratio",legend=:top)
 
 ratio(C1,C2) = C1 / C2
 Δratio(C1,C2,ΔC1,ΔC2) = ΔC1 / C2 + ΔC2 * C1 / C2 ^ 2
-N1 = L1^(6)
-N2 = L2^(6)
+N1 = L1^(3)
+N2 = L2^(3)
 s  = (N1/N2)
 
 scatter!(plt1,C1/N1, yerr=ΔC1/N1, label=L"\pi (L=%$L1)" ,marker=:rect,alpha=0.9)
 scatter!(plt1,C2/N2, yerr=ΔC2/N2, label=L"\pi (L=%$L2)" ,marker=:rect,alpha=0.9)
 scatter!(plt2,ratio.(C1,C2)/s, yerr=Δratio.(C1,C2,ΔC1,ΔC2)/s, label="" ,marker=:rect,alpha=0.9)
+
+plot!(plt2,ylims=(-0,5))
 
 display(plt2)
 #display(plt1)
