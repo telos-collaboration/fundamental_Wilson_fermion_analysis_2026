9e6828a2aa4d024cf238198e89017f7d90ad2e16
Fabian Zierler <fabian.zierler@a1.net>
2024-09-16 13:42:43 +0100

    comments

--- scripts/test_parse.jl
+++ scripts/test_parse.jl
@@ -2,7 +2,6 @@ using Pkg; Pkg.activate(".")
 using ScatteringI1
 
 #=
-#=
 pmax = 1
 file = "/home/fabian/Downloads/I1_HDF5/out_scattering_I1_L24_h4"
 h5file = "isospin1_L24_h4.hdf5"
@@ -27,15 +26,12 @@ pmax = 2
 file = "/home/fabian/Downloads/I1_HDF5/out_scattering_I1_L16_h2_p2"
 h5file = "isospin1_L16_h2_p2.hdf5"
 isospin1_to_hdf5(file,h5file,pmax;ensemble="E1")
-=#
-
 
 pmax = 1
 file = "/home/fabian/Downloads/I1_HDF5/out_scattering_I1_L16_PA_h4"
 h5file = "isospin1_L16_PA_h4.hdf5"
 isospin1_to_hdf5(file,h5file,pmax;ensemble="E1")
 
-#=
 pmax = 1
 file = "/home/fabian/Documents/Physics/Data/DataVSC/measurements/runsSp4/Lt24Ls12beta6.9m1-0.90m2-0.90/out/out_scattering_I1"
 h5file = "isospin1_T24_L12.hdf5"
