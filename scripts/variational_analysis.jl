e126f87cb71f06e1792585e5c9633daef1949c5f
Fabian Zierler <fabian.zierler@a1.net>
2024-10-25 15:11:40 +0100

    better plots after variational analysis

--- scripts/variational_analysis.jl
+++ scripts/variational_analysis.jl
@@ -5,18 +5,22 @@ using HDF5
 using Statistics
 using LaTeXStrings
 include("utils.jl")
+gr(frame=:box, legend=:topright,legendfontsize=12)
+plotlyjs(frame=:box)
+pgfplotsx(frame=:box, legend=:topright,labelfontsize=16, titlefontsize=11, legendfontsize=8,markersize=5,tickfontsize=12)
 
 hdf5file = "isospin1_L24_h16_p23.hdf5"
-hdf5file = "isospin1_L24_h16.hdf5"
+hdf5file = "isospin1_L16_PA_h4.hdf5"
 hdf5file = "isospin1_L24_h16.hdf5"
 hdf5file = "isospin1_L16_h4.hdf5"
-hdf5file = "isospin1_L16_PA_h4.hdf5"
 h5dset = h5open(hdf5file)
 
 p1  = "(0,0,1)"
 p   = 1
 ens = "E1"
 T, L = h5dset["$ens/lattice"][1:2]
+title_corr = L"$%$T \times %$L^3, \beta=6.9, m_0^f=-0.92, \mathbf p = %$p1$: Eigenvalues from GEVP "
+title_meff = L"$%$T \times %$L^3, \beta=6.9, m_0^f=-0.92, \mathbf p = %$p1$: Effective mass from GEVP"
 
 Corrπ, Corrρ, CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2 = correlatorsp001(h5dset,ens;p)
 N, nhits, T = size(CorrD1)
@@ -44,11 +48,23 @@ eigvals, Δeigvals = eigenvalues(Corr)
 eigvals_resamples = eigenvalues_jackknife_samples(Corr)
 meff, Δmeff =  meff_from_jackknife(eigvals_resamples;sign=+1,swap=nothing)
 
-plotlyjs()
-plt = plot(yscale=:log10)
-scatter!(plt,eigvals[2,:],yerr=Δeigvals[2,:])
-scatter!(plt,abs.(eigvals[1,:]),yerr=Δeigvals[1,:])
+t = 1:T
+plt_corr = plot(;title=title_corr,yscale=:log10)
+plot_correlator!(plt_corr,t,abs.(eigvals[2,:]),Δeigvals[2,:],label="Eigenvalue #1")
+plot_correlator!(plt_corr,t,abs.(eigvals[1,:]),Δeigvals[1,:],label="Eigenvalue #2")
 
-plt_meff = plot()
-scatter!(plt_meff,meff[2,:],yerr=Δmeff[2,:])
-scatter!(plt_meff,meff[1,:],yerr=Δmeff[1,:])
+plt_meff = plot(;title=title_meff)
+scatter!(plt_meff,meff[2,:],yerr=Δmeff[2,:],label="Eigenvalue #1")
+scatter!(plt_meff,meff[1,:],yerr=Δmeff[1,:],label="Eigenvalue #2")
+plot!(plt_meff,xlims=(0.5,T÷2+0.5))
+
+display(plt_corr)
+display(plt_meff)
+
+save=true
+if save
+    path = "plots/$hdf5file/"
+    ispath(path) || mkpath(path)
+    savefig(plt_corr,joinpath(path,"eigenvalues_gevp.pdf"))
+    savefig(plt_meff,joinpath(path,"meff_gevp.pdf"))
+end
