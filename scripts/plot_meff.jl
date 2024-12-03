07057431497c5943c4ba0f821a4e2e7c04b23822
Fabian Zierler <fabian.zierler@a1.net>
2024-12-03 17:26:03 +0000

    clean up meff and gevp

--- scripts/plot_meff.jl
+++ scripts/plot_meff.jl
@@ -7,36 +7,34 @@ using LaTeXStrings
 gr(frame=:box)
 include("utils.jl")
 
-hdf5file = "isospin1.hdf5"
+hdf5file = "isospin1_finer_lattices.hdf5"
 h5dset = h5open(hdf5file)
 
 p   = 1
 p1  = "(0,0,$p)"
-ens = "T32_L16_h2_p2"
-ens = "T32_L24_h16_p1"
-ens = "m0.90/T32_L12_h4_p1"
+ens = "Lt32Ls16beta7.2m1-0.794m2-0.794"
+ens = "Lt32Ls16beta7.05m1-0.85m2-0.85"
+ens = "Lt36Ls16beta7.2m1-0.76m2-0.76"
+
 T, L = h5dset["$ens/lattice"][1:2]
 title = L"$%$T \times %$L^3, \beta=6.9, m_0^f=-0.92, \mathbf p = %$p1$: Effective masses"
 
-Corrπ0, Corrρ0, CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2 = correlatorsp000(h5dset,ens;p)
-Corrπ , Corrρ , CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2 = correlatorsp001(h5dset,ens;p)
-#Corrπ , Corrρ , CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2 = correlatorsp110(h5dset,ens;p)
+Corrπ0, Corrρ0 = correlatorsp000(h5dset,ens;p)
+Corrπ , Corrρ , CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2, CorrD1_old = correlatorsp001(h5dset,ens;p)
 
 N, nhits, T = size(CorrD1)
 L3, L6 = L^3, L^6
 
 Corr2π = dropdims(mean(CorrD1/L6 - CorrD2/L6 + 2CorrR1/L3 - 2CorrR3/L3,dims=2),dims=2) # Isospin - 1
-Corr2π = dropdims(mean(CorrD1/L6 + CorrD2/L6 -  CorrR1/L3 -  CorrR3/L3,dims=2),dims=2) # Isospin - 2
-#Corr2π = correlator_derivative(Corr2π,t_dim=2)
 
 Corr1π0 = dropdims(mean(Corrπ0/L3,dims=2),dims=2)
 Corr1π  = dropdims(mean(Corrπ/L3 ,dims=2),dims=2)
 Corr1ρ  = dropdims(mean(Corrρ/L3 ,dims=2),dims=2)
 Corr1ρ0 = dropdims(mean(Corrρ0/L3 ,dims=2),dims=2)
-m2π, Δm2π   = implicit_meff_jackknife(Corr2π',sign=-1)
+m2π, Δm2π   = implicit_meff_jackknife(Corr2π')
 m1π, Δm1π   = implicit_meff_jackknife(Corr1π')
-m1π0, Δm1π0 = implicit_meff_jackknife(Corr1π0')
 m1ρ, Δm1ρ   = implicit_meff_jackknife(Corr1ρ')
+m1π0, Δm1π0 = implicit_meff_jackknife(Corr1π0')
 m1ρ0, Δm1ρ0 = implicit_meff_jackknife(Corr1ρ0')
 
 plt = plot(;title, ylabel=L"m_{\rm eff}", xlabel=L"t")
@@ -44,7 +42,7 @@ scatter!(plt,m2π,yerr=Δm2π, label="pipi")
 scatter!(plt,m1ρ0,yerr=Δm1ρ0,label="rho(0)")
 scatter!(plt,m1ρ ,yerr=Δm1ρ ,label="rho(p)")
 scatter!(plt,m1π+m1π0,yerr=Δm1π+Δm1π0,label="pi(0) + pi(p)")
-plot!(plt,ylims=(0,3),xlims=(0,T÷2+0.5),xticks=1:2:T)
+plot!(plt,xlims=(0,T÷2+0.5),xticks=1:2:T)
 
 save=true
 if save
--- scripts/variational_analysis.jl
+++ scripts/variational_analysis.jl
@@ -6,22 +6,21 @@ using Statistics
 using LaTeXStrings
 include("utils.jl")
 gr(frame=:box, legend=:topright,legendfontsize=12)
-pgfplotsx(frame=:box, legend=:topright,labelfontsize=16, titlefontsize=11, legendfontsize=8,markersize=5,tickfontsize=12)
-plotlyjs(frame=:box)
 
-hdf5file = "isospin1.hdf5"
+hdf5file = "isospin1_finer_lattices.hdf5"
 h5dset = h5open(hdf5file)
 p   = 1
-p1  = "(0,0,1)"
-ens = "m0.90/T32_L12_h4_p1"
-ens = "T32_L24_h4_p1"
-t0 = 1
+p1  = "(0,0,$p)"
+ens = "Lt32Ls16beta7.2m1-0.794m2-0.794"
+ens = "Lt32Ls16beta7.05m1-0.85m2-0.85"
+ens = "Lt36Ls16beta7.2m1-0.76m2-0.76"
+t0  = 4
 
 T, L = h5dset["$ens/lattice"][1:2]
 title_corr = L"$%$T \times %$L^3, \beta=6.9, m_0^f=-0.92, \mathbf p = %$p1$: Eigenvalues from GEVP "
 title_meff = L"$%$T \times %$L^3, \beta=6.9, m_0^f=-0.92, \mathbf p = %$p1$: Effective mass from GEVP"
 
-Corrπ, Corrρ, CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2 = correlatorsp001(h5dset,ens;p)
+Corrπ, Corrρ, CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2, CorrD1_old = correlatorsp001(h5dset,ens;p)
 N, nhits, T = size(CorrD1)
 
 function pipi_correlator(CorrD1,CorrR1,CorrD2,CorrR3,L)
@@ -52,10 +51,9 @@ plt_corr = plot(;title=title_corr,yscale=:log10)
 plot_correlator!(plt_corr,t,abs.(eigvals[2,:]),Δeigvals[2,:],label="Eigenvalue #1")
 plot_correlator!(plt_corr,t,abs.(eigvals[1,:]),Δeigvals[1,:],label="Eigenvalue #2")
 
-plt_meff = plot(;title=title_meff)
-scatter!(plt_meff,meff[2,:],yerr=Δmeff[2,:],label="Eigenvalue #1")
-scatter!(plt_meff,meff[1,:],yerr=Δmeff[1,:],label="Eigenvalue #2")
-plot!(plt_meff,xlims=(0.5,T÷2+0.5),ylims=(0,2))
-
+plt = plot(;title=title_meff)
+scatter!(plt,meff[2,:],yerr=Δmeff[2,:],label="Eigenvalue #1")
+scatter!(plt,meff[1,:],yerr=Δmeff[1,:],label="Eigenvalue #2")
+plot!(plt,xlims=(0.5,T÷2+0.5),ylims=(0,2))
 display(plt_corr)
-display(plt_meff)
+display(plt)
