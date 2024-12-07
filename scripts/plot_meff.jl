9e98b6c4b37c0fa677cf38436249926b2f64f9c1
Fabian Zierler <fabian.zierler@a1.net>
2024-12-07 19:04:52 +0000

    switch to simple effective mass

--- scripts/plot_meff.jl
+++ scripts/plot_meff.jl
@@ -4,6 +4,7 @@ using Plots
 using HDF5
 using Statistics
 using LaTeXStrings
+using LatticeUtils
 gr(frame=:box,markersize=5,markeralpha=0.75)
 include("read_rhopipi_diagrams.jl")
 
@@ -12,16 +13,8 @@ h5dset = h5open(hdf5file)
 
 p   = 1
 p1  = "(0,0,$p)"
-ens = "Lt32Ls16beta7.2m1-0.794m2-0.794"
-ens = "Lt32Ls16beta7.05m1-0.85m2-0.85"
-ens = "Lt36Ls16beta7.2m1-0.76m2-0.76"
-ens = "Lt48Ls16beta7.4m1-0.75m2-0.75"
-ens = "Lt48Ls16beta7.4m1-0.74m2-0.74"
-ens = "Lt32Ls16beta6.9m1-0.92m2-0.92"
 ens = "Lt32Ls24beta6.9m1-0.92m2-0.92"
-
 T, L = h5dset["$ens/lattice"][1:2]
-title = L"$32 \times 24^3, \beta=6.9, m_0^f=-0.92, \mathbf p = (0,0,1):$ Effective masses"
 
 Corrπ0, Corrρ0 = correlatorsp000(h5dset,ens;p)
 Corrπ , Corrρ , CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2, CorrD1_old = correlatorsp001(h5dset,ens;p)
@@ -38,17 +31,17 @@ Corr1π0 = dropdims(mean(Corrπ0/L3,dims=2),dims=2)
 Corr1π  = dropdims(mean(Corrπ/L3 ,dims=2),dims=2)
 Corr1ρ  = dropdims(mean(Corrρ/L3 ,dims=2),dims=2)
 Corr1ρ0 = dropdims(mean(Corrρ0/L3 ,dims=2),dims=2)
-m2π, Δm2π   = implicit_meff(Corr2π';sign)
-m1π, Δm1π   = implicit_meff(Corr1π')
-m1ρ, Δm1ρ   = implicit_meff(Corr1ρ')
-m1π0, Δm1π0 = implicit_meff(Corr1π0')
-m1ρ0, Δm1ρ0 = implicit_meff(Corr1ρ0')
+m2π, Δm2π   = log_meff(Corr2π')
+m1π, Δm1π   = log_meff(Corr1π')
+m1ρ, Δm1ρ   = log_meff(Corr1ρ')
+m1π0, Δm1π0 = log_meff(Corr1π0')
+m1ρ0, Δm1ρ0 = log_meff(Corr1ρ0')
 
-plt = plot(;title, ylabel=L"m_{\rm eff}", xlabel=L"t")
-#scatter!(plt,m2π     ,yerr=Δm2π      ,marker=:circ ,label="pipi")
+plt = plot(;ylabel=L"m_{\rm eff}", xlabel=L"t")
+scatter!(plt,m1π     ,yerr=Δm1π      ,marker=:circ ,label="pi(0)")
+scatter!(plt,m2π     ,yerr=Δm2π      ,marker=:circ ,label="pipi")
 scatter!(plt,m1ρ     ,yerr=Δm1ρ      ,marker=:rect ,label="rho(p)")
-scatter!(plt,m1π+m1π0,yerr=Δm1π+Δm1π0,marker=:pent ,label="pi(0) + pi(p)")
-plot!(plt,ylims=(0.5,1.5),xlims=(1.5,13),xticks=2:2:T)
+plot!(plt,ylims=(0.0,2),xlims=(1.5,T÷2),xticks=2:2:T)
 
 save=false
 if save
--- scripts/variational_analysis.jl
+++ scripts/variational_analysis.jl
@@ -12,15 +12,18 @@ hdf5file = "data/isospin1.hdf5"
 h5dset = h5open(hdf5file)
 p   = 1
 p1  = "(0,0,$p)"
-ens = "Lt48Ls16beta7.4m1-0.75m2-0.75"
-ens = "Lt48Ls16beta7.4m1-0.74m2-0.74"
-ens = "Lt36Ls16beta7.2m1-0.76m2-0.76"
-ens = "Lt32Ls16beta7.2m1-0.794m2-0.794"
-ens = "Lt32Ls16beta7.2m1-0.78m2-0.78"
-ens = "Lt32Ls16beta7.05m1-0.85m2-0.85"
-ens = "Lt32Ls16beta6.9m1-0.92m2-0.92"
-ens = "Lt32Ls24beta6.9m1-0.92m2-0.92"
-t0  = 2
+
+ens = "Lt32Ls16beta7.2m1-0.78m2-0.78"   # (more hits) 
+ens = "Lt32Ls16beta7.2m1-0.794m2-0.794" # (more hits)
+ens = "Lt48Ls16beta7.4m1-0.75m2-0.75"   # (more hits)
+ens = "Lt48Ls16beta7.4m1-0.74m2-0.74"   # (more hits)
+ens = "Lt36Ls16beta7.2m1-0.76m2-0.76"   # t0=8; deriv (more hits)
+ens = "Lt32Ls24beta6.9m1-0.92m2-0.92"   # t0=8; deriv
+ens = "Lt32Ls16beta6.9m1-0.92m2-0.92"   # t0=8; deriv (more hits)
+ens = "Lt24Ls14beta6.9m1-0.92m2-0.92"   # t0=7; deriv
+ens = "Lt32Ls16beta7.05m1-0.85m2-0.85"  # t0=8; deriv (more hits)
+maxhits = 16
+t0  = 7
 
 T, L = h5dset["$ens/lattice"][1:2]
 title_corr = L"$%$T \times %$L^3, \beta=6.9, m_0^f=-0.92, \mathbf p = %$p1$: Eigenvalues from GEVP "
@@ -31,10 +34,10 @@ N, nhits, T = size(CorrD1)
 
 function pipi_correlator(CorrD1,CorrR1,CorrD2,CorrR3,L)
     L3, L6 = L^3, L^6
-    Corr2π = + CorrD1/L6 - CorrD2/L6 + 2CorrR1/L3 - 2CorrR3/L3
+    Corr2π = @. (CorrD1 - CorrD2)/L6 + (CorrR1 + CorrR2 - CorrR3 - CorrR4)/L3
     return Corr2π 
 end
-function pipi_rho_matrix(Corr2π,Corrρ,CorrT1,CorrT2,L)
+function pipi_rho_matrix(Corr2π,Corrρ,CorrT1,CorrT2,L;maxhits=typemax(Int))
     N, nhits, T = size(Corr2π)
     L3, L6 = L^3, L^6
     corr = zeros(ComplexF64,(2,2,N,nhits,T))
@@ -42,23 +45,24 @@ function pipi_rho_matrix(Corr2π,Corrρ,CorrT1,CorrT2,L)
     corr[1,2,:,:,:] =  @. 0        + im*(CorrT1-CorrT2)/L3
     corr[2,1,:,:,:] =  @. 0        + im*(CorrT2-CorrT1)/L3
     corr[2,2,:,:,:] =  @. Corr2π   + 0*im
-    corr = dropdims(mean(corr,dims=4),dims=4)
+    h = min(nhits,maxhits)
+    corr = dropdims(mean(corr[:,:,:,1:h,:],dims=4),dims=4)
     return corr
 end
-Corr2π = pipi_correlator(CorrD1,CorrR1,CorrD2,CorrR3,L)
-Corr   = pipi_rho_matrix(Corr2π,Corrρ,CorrT1,CorrT2,L)
+
 sign   = +1
+Corr2π = pipi_correlator(CorrD1,CorrR1,CorrD2,CorrR3,L)
+Corr   = pipi_rho_matrix(Corr2π,Corrρ,CorrT1,CorrT2,L;maxhits)
 Corr   = correlator_derivative(Corr;t_dim=4)
 sign   = -1
 
 eigvals, Δeigvals = eigenvalues(Corr;t0)
 eigvals_resamples = eigenvalues_jackknife_samples(Corr;t0)
-meff, Δmeff =  LatticeUtils.implicit_meff_jackknife(real.(eigvals_resamples);sign)
+meff, Δmeff =  LatticeUtils.log_meff_jackknife(real.(eigvals_resamples))
 
-r = 1:15
-#plt = plot(;title=title_meff)
+plt = plot(;title=title_meff)
 scatter!(plt,meff[2,:],yerr=Δmeff[2,:],label="Eigenvalue #1")
-scatter!(plt,meff[1,r],yerr=Δmeff[1,r],label="Eigenvalue #2")
-plot!(plt,ylims=(0.5,1.5),xlims=(1.5,13),xticks=2:2:T)
+scatter!(plt,meff[1,:],yerr=Δmeff[1,:],label="Eigenvalue #2")
+plot!(plt,ylims=(0.0,2),xlims=(1.5,T÷2),xticks=2:2:T)
 display(plt)
 savefig(plt,joinpath("gevp.pdf"))
