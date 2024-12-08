92c637b21fb6cef61aea9c5e2343599547c4d762
Fabian Zierler <fabian.zierler@a1.net>
2024-12-08 11:39:09 +0000

    test more ensembles

--- scripts/test_parse.jl
+++ scripts/test_parse.jl
@@ -1,23 +1,32 @@
 using Pkg; Pkg.activate(".")
 using ScatteringI1
 
-h5file = "data/isospin1.hdf5"
+h5file = "data/isospin1_new.hdf5"
 path   = "/home/fabian/Documents/Physics/Data/DataVSC/measurements/"
 path   = "/home/fabian/Dokumente/Physics/Data/DataVSC/measurements/"
+log    = "out/out_scattering_I1"
 names  = [
     "runsSp4/Lt24Ls14beta6.9m1-0.92m2-0.92",
     #"runsSp4/Lt32Ls16beta6.9m1-0.92m2-0.92",
-    #"runsSp4/Lt32Ls16beta7.05m1-0.85m2-0.85",
-    #"runsSp4/Lt32Ls16beta7.2m1-0.78m2-0.78",
-    #"runsSp4/Lt32Ls16beta7.2m1-0.794m2-0.794",
-    #"runsSp4/Lt48Ls16beta7.4m1-0.74m2-0.74",
-    #"runsSp4/Lt48Ls16beta7.4m1-0.75m2-0.75",
-    #"Pusan/Lt32Ls24beta6.9m1-0.92m2-0.92",
-    #"Pusan/Lt36Ls16beta7.2m1-0.76m2-0.76",
+    "runsSp4/Lt32Ls16beta7.05m1-0.85m2-0.85",
+    "runsSp4/Lt32Ls16beta7.2m1-0.78m2-0.78",
+    "runsSp4/Lt32Ls16beta7.2m1-0.794m2-0.794",
+    "runsSp4/Lt48Ls16beta7.4m1-0.74m2-0.74",
+    "runsSp4/Lt48Ls16beta7.4m1-0.75m2-0.75",
+    "Pusan/Lt32Ls24beta6.9m1-0.92m2-0.92",
+    "Pusan/Lt36Ls16beta7.2m1-0.76m2-0.76",
 ]
 
+#h5file = "data/isospin1_PA.hdf5"
+#path   = "/home/fabian/Documents/Physics/Data/DataVSC/measurements/"
+#path   = "/home/fabian/Dokumente/Physics/Data/DataVSC/measurements/"
+#log    = "out/out_scattering_I1_PA"
+#names  = [
+#    "runsSp4/Lt24Ls14beta6.9m1-0.92m2-0.92",
+#]
+
 for name in names
-    file = joinpath(path,"$name/out/out_scattering_I1")
+    file = joinpath(path,"$name/$log")
     ens  = basename(name)
     @show name
     h5file == "test.hdf5" && isfile(h5file) && rm(h5file)
--- scripts/variational_analysis.jl
+++ scripts/variational_analysis.jl
@@ -7,28 +7,39 @@ using LaTeXStrings
 using LatticeUtils
 include("read_rhopipi_diagrams.jl")
 gr(frame=:box, legend=:topright,legendfontsize=12)
+function non_interacting_energy(mπ,Δmπ,p2,L)
+    E1  = sqrt(mπ^2 + p2*(2*pi/L)^2)
+    ΔE1 = Δmπ*mπ/E1
+    E   = E1 + mπ
+    ΔE  = sqrt(Δmπ^2 + ΔE1^2)
+    return E, ΔE
+end
 
-hdf5file = "data/isospin1.hdf5"
+hdf5file = "data/isospin1_new.hdf5"
 h5dset = h5open(hdf5file)
 p   = 1
 p1  = "(0,0,$p)"
 
-ens = "Lt32Ls16beta7.2m1-0.78m2-0.78"   # (more hits) 
-ens = "Lt32Ls16beta7.2m1-0.794m2-0.794" # (more hits)
-ens = "Lt48Ls16beta7.4m1-0.75m2-0.75"   # (more hits)
 ens = "Lt48Ls16beta7.4m1-0.74m2-0.74"   # (more hits)
-ens = "Lt36Ls16beta7.2m1-0.76m2-0.76"   # t0=8; deriv (more hits)
-ens = "Lt32Ls24beta6.9m1-0.92m2-0.92"   # t0=8; deriv
+ens = "Lt48Ls16beta7.4m1-0.75m2-0.75"   # (more hits)
+ens = "Lt32Ls16beta7.2m1-0.794m2-0.794" # (more hits)
+
+ens = "Lt32Ls16beta7.2m1-0.78m2-0.78"   # t0=8; deriv; (below?) 
+mπ,Δmπ = 0.36963, 0.00039
+ens = "Lt36Ls16beta7.2m1-0.76m2-0.76"   # t0=8; deriv; (bad signal, larger L?)
+mπ,Δmπ = 0.45700, 0.00130
+ens = "Lt32Ls16beta7.05m1-0.85m2-0.85"  # t0=8; deriv; (bad signal, larger L?)
+mπ,Δmπ = 0.33076, 0.00097
+
 ens = "Lt32Ls16beta6.9m1-0.92m2-0.92"   # t0=8; deriv (more hits)
-ens = "Lt24Ls14beta6.9m1-0.92m2-0.92"   # t0=7; deriv
-ens = "Lt32Ls16beta7.05m1-0.85m2-0.85"  # t0=8; deriv (more hits)
+ens = "Lt24Ls14beta6.9m1-0.92m2-0.92"   # t0=7; deriv (PA?)
+ens = "Lt32Ls24beta6.9m1-0.92m2-0.92"   # t0=8; deriv
+mπ,Δmπ = 0.38649, 0.00051
+
 maxhits = 16
-t0  = 7
+t0  = 8
 
 T, L = h5dset["$ens/lattice"][1:2]
-title_corr = L"$%$T \times %$L^3, \beta=6.9, m_0^f=-0.92, \mathbf p = %$p1$: Eigenvalues from GEVP "
-title_meff = L"$%$T \times %$L^3, \beta=6.9, m_0^f=-0.92, \mathbf p = %$p1$: Effective mass from GEVP"
-
 Corrπ, Corrρ, CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2, CorrD1_old = correlatorsp001(h5dset,ens;p)
 N, nhits, T = size(CorrD1)
 
@@ -53,6 +64,7 @@ end
 sign   = +1
 Corr2π = pipi_correlator(CorrD1,CorrR1,CorrD2,CorrR3,L)
 Corr   = pipi_rho_matrix(Corr2π,Corrρ,CorrT1,CorrT2,L;maxhits)
+Corr   = correlator_folding(Corr;t_dim=4,sign)
 Corr   = correlator_derivative(Corr;t_dim=4)
 sign   = -1
 
@@ -60,9 +72,13 @@ eigvals, Δeigvals = eigenvalues(Corr;t0)
 eigvals_resamples = eigenvalues_jackknife_samples(Corr;t0)
 meff, Δmeff =  LatticeUtils.log_meff_jackknife(real.(eigvals_resamples))
 
-plt = plot(;title=title_meff)
+plt = plot()
 scatter!(plt,meff[2,:],yerr=Δmeff[2,:],label="Eigenvalue #1")
 scatter!(plt,meff[1,:],yerr=Δmeff[1,:],label="Eigenvalue #2")
-plot!(plt,ylims=(0.0,2),xlims=(1.5,T÷2),xticks=2:2:T)
+plot!(plt,ylims=(0.0,1.5),xlims=(1.5,T÷2),xticks=2:2:T)
+for p2 in 1:2
+    add_mass_band!(plt,non_interacting_energy(mπ,10Δmπ,p2,L)...)
+end
+
 display(plt)
-savefig(plt,joinpath("gevp.pdf"))
+#savefig(plt,joinpath("gevp.pdf"))
