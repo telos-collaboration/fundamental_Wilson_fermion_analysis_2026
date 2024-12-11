bf1cf14d038c9449df77d7e7243b0aef185f6f7d
Fabian Zierler <fabian.zierler@a1.net>
2024-12-11 12:18:33 +0000

    more ensembles, nicer plotting

--- scripts/variational_analysis.jl
+++ scripts/variational_analysis.jl
@@ -6,7 +6,7 @@ using Statistics
 using LaTeXStrings
 using LatticeUtils
 include("read_rhopipi_diagrams.jl")
-gr(frame=:box, legend=:topright,legendfontsize=12)
+pgfplotsx(frame=:box,markersize=5,labelfontsize=16,tickfontsize=14,legendfontsize=14,legend=:bottomleft,markeralpha=0.7)
 function non_interacting_energy(mπ,Δmπ,p2,L)
     E1  = sqrt(mπ^2 + p2*(2*pi/L)^2)
     ΔE1 = Δmπ*mπ/E1
@@ -15,31 +15,39 @@ function non_interacting_energy(mπ,Δmπ,p2,L)
     return E, ΔE
 end
 
-hdf5file = "data/isospin1_new.hdf5"
+hdf5file = "data/isospin1.hdf5"
 h5dset = h5open(hdf5file)
 p   = 1
 p1  = "(0,0,$p)"
+t1_max = 16 
+t2_max = 14
 
 ens = "Lt48Ls16beta7.4m1-0.74m2-0.74"   # (more hits)
 ens = "Lt48Ls16beta7.4m1-0.75m2-0.75"   # (more hits)
-ens = "Lt32Ls16beta7.2m1-0.794m2-0.794" # (more hits)
 
-ens = "Lt32Ls16beta7.2m1-0.78m2-0.78"   # t0=8; deriv; (below?) 
-mπ,Δmπ = 0.36963, 0.00039
-ens = "Lt36Ls16beta7.2m1-0.76m2-0.76"   # t0=8; deriv; (bad signal, larger L?)
+ens = "Lt32Ls16beta7.2m1-0.794m2-0.794" # (more configs?)
+mπ,Δmπ = 0.2532, 0.0007
+ens = "Lt36Ls16beta7.2m1-0.76m2-0.76"    # t0=8; deriv (bad signal,larger L?)
 mπ,Δmπ = 0.45700, 0.00130
-ens = "Lt32Ls16beta7.05m1-0.85m2-0.85"  # t0=8; deriv; (bad signal, larger L?)
+ens = "Lt32Ls24beta6.9m1-0.924m2-0.924"
+mπ,Δmπ = 0.33880, 0.00120
+ens = "Lt36Ls20beta7.05m1-0.835m2-0.835" # t0=8; deriv
+mπ,Δmπ = 0.43800, 0.00100
+ens = "Lt24Ls14beta7.05m1-0.85m2-0.85"   # t0=8; deriv; (bad signal,larger L?)
+ens = "Lt32Ls16beta7.05m1-0.85m2-0.85"   # t0=8; deriv; (bad signal,larger L?)
 mπ,Δmπ = 0.33076, 0.00097
-
-ens = "Lt32Ls16beta6.9m1-0.92m2-0.92"   # t0=8; deriv (more hits)
-ens = "Lt24Ls14beta6.9m1-0.92m2-0.92"   # t0=7; deriv (PA?)
-ens = "Lt32Ls24beta6.9m1-0.92m2-0.92"   # t0=8; deriv
+ens = "Lt32Ls24beta6.9m1-0.92m2-0.92"    # t0=8; deriv
+ens = "Lt24Ls14beta6.9m1-0.92m2-0.92"    # t0=7; deriv (ok signal, better meff)
+ens = "Lt32Ls16beta6.9m1-0.92m2-0.92"    # t0=8; deriv (bad signal,larger L?)
 mπ,Δmπ = 0.38649, 0.00051
+ens = "Lt32Ls16beta7.2m1-0.78m2-0.78"    # t0=8; deriv (below?) 
+mπ,Δmπ = 0.36963, 0.00039
 
-maxhits = 16
+maxhits = 64
 t0  = 8
 
 T, L = h5dset["$ens/lattice"][1:2]
+m0 = -parse(Float64,last(split(ens,'-')))
 Corrπ, Corrρ, CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2, CorrD1_old = correlatorsp001(h5dset,ens;p)
 N, nhits, T = size(CorrD1)
 
@@ -70,15 +78,16 @@ sign   = -1
 
 eigvals, Δeigvals = eigenvalues(Corr;t0)
 eigvals_resamples = eigenvalues_jackknife_samples(Corr;t0)
-meff, Δmeff =  LatticeUtils.log_meff_jackknife(real.(eigvals_resamples))
+meff, Δmeff = LatticeUtils.log_meff_jackknife(real.(eigvals_resamples))
 
-plt = plot()
-scatter!(plt,meff[2,:],yerr=Δmeff[2,:],label="Eigenvalue #1")
-scatter!(plt,meff[1,:],yerr=Δmeff[1,:],label="Eigenvalue #2")
-plot!(plt,ylims=(0.0,1.5),xlims=(1.5,T÷2),xticks=2:2:T)
+plt = plot(ylabel="effective mass",xlabel=L"t",title=L"${%$T} \times {%$L}^3: am^f_0={%$m0}, J^P = 1^-$, ops$ = \pi(\mathbf p)\pi(\mathbf 0), \rho(\mathbf p), \mathbf p=(0,0,1)$")
+scatter!(plt,meff[2,1:t1_max],yerr=Δmeff[2,1:t1_max],label="eigenvalue #1")
+scatter!(plt,meff[1,1:t2_max],yerr=Δmeff[1,1:t2_max],label="eigenvalue #2")
+plot!(plt,ylims=(0.2,1.2),xlims=(1.5,T÷2),xticks=2:2:T)
 for p2 in 1:2
-    add_mass_band!(plt,non_interacting_energy(mπ,10Δmπ,p2,L)...)
+    label = p2 == 1 ? L"non-interacting $E_{\pi(\mathbf p)\pi(\mathbf 0)}$" : "" 
+    add_mass_band!(plt,non_interacting_energy(mπ,10Δmπ,p2,L)...;label)
 end
 
 display(plt)
-#savefig(plt,joinpath("gevp.pdf"))
+savefig(plt,joinpath("gevp_$ens.pdf"))
