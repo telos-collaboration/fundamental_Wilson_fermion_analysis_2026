d1728670ac56cd2b396bfe044f0f0aee43a37576
Fabian Zierler <fabian.zierler@a1.net>
2025-02-19 14:01:28 +0000

    add csv files containing info on all input files

--- scripts/test_parse.jl
+++ scripts/test_parse.jl
@@ -1,32 +1,17 @@
 using Pkg; Pkg.activate(".")
 using ScatteringI1
+using DelimitedFiles
 
 h5file = "data/isospin1.hdf5"
-path   = "/home/fabian/Dokumente/Physics/Data/DataVSC/measurements/"
 path   = "/home/fabian/Documents/Physics/Data/DataVSC/measurements/"
-log    = "out/out_scattering_I1"
-names  = [
-    #"runsSp4/Lt24Ls14beta6.9m1-0.92m2-0.92",
-    "runsSp4/Lt32Ls16beta6.9m1-0.92m2-0.92",
-    "Pusan/Lt32Ls24beta6.9m1-0.92m2-0.92",
-    #"runsSp4/Lt24Ls14beta7.05m1-0.85m2-0.85",
-    #"runsSp4/Lt32Ls16beta7.05m1-0.85m2-0.85",
-    #"runsSp4/Lt32Ls16beta7.2m1-0.78m2-0.78",
-    #"runsSp4/Lt48Ls16beta7.4m1-0.74m2-0.74",
-    #"Pusan/Lt36Ls16beta7.2m1-0.76m2-0.76",
-    #"Pusan/Lt32Ls24beta6.9m1-0.924m2-0.924",
-    #"runsSp4/Lt48Ls16beta7.4m1-0.75m2-0.75",
-    #"runsSp4/Lt32Ls16beta7.2m1-0.794m2-0.794",
-    #"runsSp4/Lt36Ls24beta7.05m1-0.867m2-0.867",
-    #"Pusan/Lt36Ls20beta7.05m1-0.835m2-0.835",
-]
-
+path   = "/home/fabian/Dokumente/Physics/Data/DataVSC/measurements/"
+info   = readdlm("input/input_files.csv",',',skipstart=1)
 single_file = true
 
-for name in names
-    file = joinpath(path,"$name/$log")
-    ens  = basename(name)
-    @show name
+for (name,dir,file,run) in eachrow(info)
+
+    file = joinpath(path,dir,name,file)
+    ens  = joinpath(name,run)
 
     if single_file 
         h5file == "test.hdf5" && isfile(h5file) && rm(h5file)
@@ -34,13 +19,7 @@ for name in names
     else
         dir = joinpath(dirname(h5file),"ensembles")
         ispath(dir) || mkpath(dir) 
-        f = joinpath(dir,ens*".hdf5")
-        isospin1_to_hdf5(file,f;ensemble="",setup=true)
+        f = joinpath(dir,name*".hdf5")
+        isospin1_to_hdf5(file,f;ensemble=run,setup=true)
     end
 end
-
-using HDF5
-f = h5open(h5file)
-ensembles = keys(f)
-entries   = keys(f[ensembles[3]])
-println.(entries)
