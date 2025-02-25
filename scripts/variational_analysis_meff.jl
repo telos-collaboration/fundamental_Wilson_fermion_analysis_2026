5be57627237745511e1fdf6bdef2b47a3e464e3b
Fabian Zierler <fabian.zierler@a1.net>
2025-02-25 15:12:58 +0000

    plot all effective masses

--- scripts/variational_analysis_meff.jl
+++ scripts/variational_analysis_meff.jl
@@ -36,43 +36,51 @@ function effective_masses(Corr;t0,maxhits=typemax(Int))
     
     return meff1, Δmeff1, meff2, Δmeff2, h
 end
-function plot_effective_masses!(plt, meff, Δmeff, h, T, L, m0, t0, mπ, Δmπ, mρ, Δmρ, p; t1_max=T÷2, t2_max=T÷2,all_non_interacting=false)
-    plot!(plt,ylabel=L"effective mass $[a^{-1}]$",xlabel=L"t",title=L"${%$T} \times {%$L}^3: am^f_0={%$m0}, J^P = 1^-$, ops$ = \pi(\mathbf p)\pi(\mathbf 0), \rho(\mathbf p), %$(p), n_{src}=%$h, t_0 = %$(t0)$")
+function plot_effective_masses!(plt, meff, Δmeff, h, T, L, m0, t0, mπ, Δmπ, mρ, Δmρ, p, ncfg; t1_max=T÷2, t2_max=T÷2,all_non_interacting=false)
+    plot!(plt,ylabel=L"effective mass $[a^{-1}]$",xlabel=L"t",title=L"${%$T} \times {%$L}^3: am^f_0={%$m0}, J^P = 1^-$, ops$ = \pi(\mathbf p)\pi(\mathbf 0), \rho(\mathbf p), %$(p), n_{src}=%$h, n_{cfg}=%$ncfg, t_0 = %$(t0)$")
     scatter!(plt,meff[2,1:t1_max],yerr=Δmeff[2,1:t1_max],label="eigenvalue #1")
     scatter!(plt,meff[1,1:t2_max],yerr=Δmeff[1,1:t2_max],label="eigenvalue #2")
-    plot!(plt,ylims=(0.2,1.5),xlims=(1.5,T÷2),xticks=2:2:T)
+    plot!(plt,ylims=(0.2,1.5),xlims=(1.5,T÷2+0.5),xticks=2:2:T)
     p2 = sum(x->x^2,[parse(Int,c) for c in filter(isdigit,p)])
     label2π  = L"n.i. $E[\pi(\mathbf p)\pi(\mathbf 0)]$ (err x10)" 
     add_mass_band!(plt,non_interacting_energy_2π(mπ,10Δmπ,p2,L)...;label=label2π)
+    label1ρ  = L"n.i. $E[\rho(\mathbf p)]$ (err x2)" 
+    add_mass_band!(plt,non_interacting_energy_1π(mρ,2Δmρ,p2,L)...;color=:orange,label=label1ρ)
     if all_non_interacting
         label1π  = L"n.i. $E[\pi(\mathbf p)]$ (err x10)" 
         label1π0 = L"n.i. $E[\pi(\mathbf 0)]$ (err x10)" 
-        label1ρ  = L"n.i. $E[\rho(\mathbf p)]$ (err x2)" 
         label1ρ0 = L"n.i. $E[\rho(\mathbf 0)]$ (err x2)" 
         add_mass_band!(plt,non_interacting_energy_1π(mπ,10Δmπ,p2,L)...;color=:black,label=label1π)
         add_mass_band!(plt,non_interacting_energy_1π(mπ,10Δmπ,0 ,L)...;color=:black,label=label1π0)
-        add_mass_band!(plt,non_interacting_energy_1π(mρ,2Δmρ,p2,L)...;color=:orange,label=label1ρ)
         add_mass_band!(plt,non_interacting_energy_1π(mρ,2Δmρ,0 ,L)...;color=:orange,label=label1ρ0)
     end
 end
+function meff_from_gevp(h5dset,ens,p,t0)
+    T, L = h5dset[joinpath("$ens","lattice")][1:2]
+    m0   = h5dset[joinpath("$ens","quarkmasses")][1]
+    β    = h5dset[joinpath("$ens","beta")][]
+    ncfg = h5dset[joinpath("$ens","Nconf")][]
+    Corr = h5dset[joinpath(ens,p,"correlation_matrix")][]
+
+    inf_vol = readdlm("input/infinite_volume.csv",',',skipstart=1)
+    ind = findfirst(i -> [β,m0] == inf_vol[i,1:2],eachindex(inf_vol))
+    mπ, Δmπ, mρ, Δmρ = inf_vol[ind,3:6]
+
+    meff, Δmeff, meff2, Δmeff2, h = effective_masses(Corr;maxhits=typemax(Int),t0)
+
+    plt = plot(legend=:outerright)
+    plot_effective_masses!(plt, meff2, Δmeff2, h, T, L, m0, t0, mπ, Δmπ, mρ, Δmρ, p, ncfg; t1_max=T÷2,t2_max=T÷2)
+    return plt
+end
 
 hdf5file = "data/isospin1_corr.hdf5"
-h5dset = h5open(hdf5file)
-ens = "Lt32Ls16beta6.9m1-0.92m2-0.92/"
-t0 = 8
+h5dset   = h5open(hdf5file)
+t0       = 6
 
-p    = "p(0,0,1)"
-T, L = h5dset[joinpath("$ens","lattice")][1:2]
-m0   = h5dset[joinpath("$ens","quarkmasses")][1]
-β    = h5dset[joinpath("$ens","beta")][]
-Corr = h5dset[joinpath(ens,p,"correlation_matrix")][]
-
-inf_vol = readdlm("input/infinite_volume.csv",',',skipstart=1)
-ind = findfirst(i -> [β,m0] == inf_vol[i,1:2],eachindex(inf_vol))
-mπ, Δmπ, mρ, Δmρ = inf_vol[ind,3:6]
-
-meff, Δmeff, meff2, Δmeff2, h = effective_masses(Corr;maxhits=typemax(Int),t0)
-
-plt = plot(legend=:outerright)
-plot_effective_masses!(plt, meff, Δmeff, h, T, L, m0, t0, mπ, Δmπ, mρ, Δmρ, p; t1_max=T÷2,t2_max=T÷2)
-display(plt)
+for ens in keys(h5dset) 
+    for p in read(h5dset[ens],"p_external")
+        p == "p(0,0,0)" && continue
+        plt = meff_from_gevp(h5dset,ens,p,t0)
+        display(plt)
+    end
+end
