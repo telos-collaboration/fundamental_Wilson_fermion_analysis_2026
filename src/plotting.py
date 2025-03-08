0dac586e25f8abf52dba3f65d2e3d9b081737e57
Fabian Zierler <fabian.zierler@a1.net>
2025-03-08 16:50:55 +0000

    remove unused comments

--- src/plotting.py
+++ src/plotting.py
@@ -55,70 +55,6 @@ def colorf(en_lv_ind):
     else:
         return "red"
 
-# def plot_E(file, show=False, save = True, which = "aE", pref = ""):                         # not up to date
-#     plt.rcParams['figure.figsize'] = [10, 6]
-#     fontsize = 14
-#     font = {'size'   : fontsize}
-#     matplotlib.rc('font', **font)
-#     fig, ax = plt.subplots()
-#     res,  res_sample =read_from_hdf(file)
-#     num_gaussian = len(res_sample["En"])
-
-#     mpi = res["mpi"]
-#     mrho = res["mrho"]
-#     N_Ls = res["N_L"]
-#     lvls = res["en_lv"]
-#     dvecs = res["dvec"]
-#     ds = res["d"]
-#     d2s = res["d2"]
-
-#     ax.set_xlabel("N_L")
-#     ax.grid()
-
-#     xarr = np.linspace(min(N_Ls)-1, max(N_Ls)+1)
-#     if which == "aE":
-#         ax.set_ylabel("aE")
-#         ax.set_ylim([0.5,1.1])
-#         ax.set_ylabel("aE")
-#         pipi1arr = [mpi*en_L(x,1,mpi) for x in xarr]
-#         pipi2arr = [mpi*en_L(x,2,mpi) for x in xarr]
-#         rho1arr = [mpi*en_rho_L(x,1,mrho,mpi) for x in xarr]
-#         rho2arr = [mpi*en_rho_L(x,2,mrho,mpi) for x in xarr]
-#         ax.axhline(mpi*2, color = "green", label = "pipi")
-#         ax.axhline(mpi*4, color = "green", lw = 2)
-#         ax.axhline(mrho, color = "orange", label = "rho")
-#         ax.plot(xarr, pipi1arr, ls = "dotted", color = "green")
-#         ax.plot(xarr, pipi2arr, ls = "dashdot", color = "green")
-#         ax.plot(xarr, rho1arr, ls = "dotted", color = "orange")
-#         ax.plot(xarr, rho2arr, ls = "dashdot", color = "orange")
-        
-#         y_plot = error_of_array(res["resampling"])(res_sample["En"])
-#     elif which == "sqrt_s":
-#         ax.set_ylim([1.9,2.4])
-#         ax.set_ylabel("$\sqrt{s}$/$m_\pi$")
-#         pipi11arr = [sqrt_s_pi_L(x,1,1) for x in xarr]
-#         # pipi12arr = [sqrt_s_pi_L(x,2,1) for x in xarr]
-#         # pipi21arr = [sqrt_s_pi_L(x,1,2) for x in xarr]
-#         pipi22arr = [sqrt_s_pi_L(x,2,2) for x in xarr]
-#         ax.axhline(2, color = "green", label = "pipi")
-#         ax.axhline(mrho/mpi, color = "orange", label = "rho")
-#         ax.plot(xarr, pipi11arr, ls = "dotted", color = "green")
-#         # ax.plot(xarr, pipi12arr, ls = "dashdot", color = "green")
-#         # ax.plot(xarr, pipi21arr, ls = "dotted", color = "green")
-#         ax.plot(xarr, pipi22arr, ls = "dashdot", color = "green")
-        
-#         y_plot = error_of_array(res["resampling"])(res_sample["E_cm_prime"])
-#     for i in range(len(y_plot[0])):
-#         ax.errorbar(N_Ls[i], y_plot[0][i], yerr = [[y_plot[1][i],],[y_plot[2][i],]], capsize=3, ls="", marker = ms_P(dvecs[i]), color = color_NL(N_Ls[i]), markersize = ms_size(lvls[i]), label = "Lv: %i, |P|^2=%i, NL=%i"%(lvls[i],d2s[i],N_Ls[i]))
-   
-#     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-#     if save:
-#         fig.savefig("output/plots/%s_L.pdf"%which, bbox_inches='tight')
-#     if show:
-#         plt.show()
-#     fig.clf()
-
-
 def color_NL(NL):
     if NL == 14:
         return "red"
@@ -181,7 +117,6 @@ def p3_cot_PS(file, show=False, save = True, pref = "", x_ax = "sqrt_s", y_ax = 
         x_plot = res["s"+ld+prime]
         x_plot_sam = np.transpose(res_sample["s"+ld+prime])
         ax.set_xlim([0.1,0.4])
-        # ax.set_xlim([4,7])
     elif x_ax == "pstar2":
         plt.xlabel(r"$p^{\star^2}"+mpi_pr_s+"^2$")
         x_plot = res["p2star"+ld+prime]
@@ -207,13 +142,11 @@ def p3_cot_PS(file, show=False, save = True, pref = "", x_ax = "sqrt_s", y_ax = 
         y_plot = np.real(res["p3cotPS_Ecm"+ld+prime])
         y_plot_sam = np.transpose(np.real(res_sample["p3cotPS_Ecm"+ld+prime]))
         plt.ylabel(r"$p^3\, \cot(\delta)/E_CM"+mpi_pr_s+"^3$")    
-        # ax.set_ylim([-0.15,0.05])
         ax.set_ylim([-1,0])
     elif y_ax == "sigma":
         y_plot = np.real(res["sigma"+ld+prime])
         y_plot_sam = np.transpose(np.real(res_sample["sigma"+ld+prime]))
         plt.ylabel(r"$\sigma_1*"+mpi_pr_s+"^2$")    
-        # ax.set_ylim([-0.15,0.05])
     elif y_ax == "cot_PS":
         y_plot = np.real(res["cot_PS"+ld])
         y_plot_sam = np.transpose(np.real(res_sample["cot_PS"+ld]))
@@ -235,7 +168,6 @@ def p3_cot_PS(file, show=False, save = True, pref = "", x_ax = "sqrt_s", y_ax = 
 
     length = len(x_plot_sam[0])
 
-
     num_perc = math.erf(1/np.sqrt(2))
     for i in range(len(N_Ls)):
         if lvls[i] != 1:
@@ -243,10 +175,8 @@ def p3_cot_PS(file, show=False, save = True, pref = "", x_ax = "sqrt_s", y_ax = 
             if bytes.decode(res["resampling"]) == "gauss":
                 sorted_indices = np.argsort(x_plot_sam[i])  
                 ax.plot(x_plot_sam[i][sorted_indices][math.floor(length*(1-num_perc)/2):math.ceil(length*(1+num_perc)/2)],delete_steps(y_plot_sam[i][sorted_indices],delete=delete)[math.floor(length*(1-num_perc)/2):math.ceil(length*(1+num_perc)/2)], color = color_NL(N_Ls[i]), ls = ls_P(dvecs[i]))
-                # ax.plot(x_plot_sam[i][sorted_indices][math.floor(length*(1-num_perc)/2):math.ceil(length*(1+num_perc)/2)],y_plot_sam[i][sorted_indices][math.floor(length*(1-num_perc)/2):math.ceil(length*(1+num_perc)/2)], color = color_NL(N_Ls[i]), ls = ls_P(dvecs[i]))
             elif bytes.decode(res["resampling"]) == "lin":
                 ax.plot(x_plot_sam[i],delete_steps(y_plot_sam[i],delete=delete), color = color_NL(N_Ls[i]), ls = ls_P(dvecs[i]))
-                # ax.plot(x_plot_sam[i],y_plot_sam[i], color = color_NL(N_Ls[i]), ls = ls_P(dvecs[i]))
 
     ax.legend(loc="best")
     if save:
@@ -278,7 +208,6 @@ def print_Lang_Prelovsek_table(file, ld = ""):
     for i in range(len(res["En"])):
         print("%f\t%f\t%f\t%f\t%f"%(E_n[i],p_star[i],s[i],PS[i].real,PS[i].imag))
     print()
-
 
 if __name__ == "__main__":
     args = sys.argv
