7ce694639832c87dc440a8e53a6161ef4449f3a4
Fabian Zierler <fabian.zierler@a1.net>
2025-03-08 11:52:53 +0000

    make compilation of zeta function work from arbitrary directories

--- zeta/compile.sh
+++ zeta/compile.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-cd zeta/get_wlm/main
+cd $(dirname "$0")/get_wlm/main
 
 make clean
 make -f Makefile_shared
 make -f Makefile_orig
-cd ../..
 
-cp get_wlm/main/get_wlm.so out/get_wlm.so
-cp get_wlm/main/get_wlm.exe out/get_wlm.exe
+cp get_wlm.so  ../../out/get_wlm.so
+cp get_wlm.exe ../../out/get_wlm.exe
+cd -
