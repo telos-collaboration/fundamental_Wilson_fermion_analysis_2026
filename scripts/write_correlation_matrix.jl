b3c4a56e88634d39d806e69869b7b8f2d3273390
Fabian Zierler <fabian.zierler@a1.net>
2025-06-19 11:06:23 +0100

    write vector correlator in other irreps to file

--- scripts/write_correlation_matrix.jl
+++ scripts/write_correlation_matrix.jl
@@ -52,9 +52,6 @@ function write_correlation_matrix(file_in,file_out;combined=true,plotting=true,p
                 # checking if the parsed data contains the first relevant dataset
                 three_by_three = haskey(fid,"$ens/p($(pv[1]),$(pv[2]),$(pv[3]))/rho_g0g1_g1/p_diag($(pv[1]),$(pv[2]),$(pv[3]))/C_re")
                 Corrπ, Corrρ, CorrT1, CorrT2, CorrR1, CorrR2, CorrR3, CorrR4, CorrD1, CorrD2 = correlators_xyz(fid,ens;p=pv)
-                Corrρ_E  = ScatteringI1.correlatorE(fid,ens;p=pv)
-                Corrρ_B1 = ScatteringI1.correlatorB1(fid,ens;p=pv)
-                Corrρ_B2 = ScatteringI1.correlatorB2(fid,ens;p=pv)
                 
                 Corr2π = pipi_correlator(CorrD1,CorrD2,CorrR1,CorrR2,CorrR3,CorrR4,L)
                 Corr   = pipi_rho_matrix(Corr2π,Corrρ,CorrT1,CorrT2,L)
@@ -67,6 +64,16 @@ function write_correlation_matrix(file_in,file_out;combined=true,plotting=true,p
                     Corr_3x3_ext = ScatteringI1.pipi_rho_matrix_3x3_extension(Corr_γ0γi_γi, Corr_γi_γ0γi, Corr_γ0γi_γ0γi, Corrγ0γiT1, Corrγ0γiT2,L)
                     h5write(file_out,joinpath(ens,p0,"Nsrc_3x3"),size(Corr_3x3_ext)[2])
                     h5write(file_out,joinpath(ens,p0,"correlation_matrix_3x3_ext"),Corr_3x3_ext)
+                end
+
+                # Skip this for p_i > 1 since we are not yet sure about the normalization
+                if maximum(pv) < 2
+                    Corrρ_E  = ScatteringI1.correlatorE(fid,ens;p=pv)
+                    Corrρ_B1 = ScatteringI1.correlatorB1(fid,ens;p=pv)
+                    Corrρ_B2 = ScatteringI1.correlatorB2(fid,ens;p=pv)
+                    isnothing(Corrρ_E)  || h5write(file_out,joinpath(ens,p0,"E"),Corrρ_E)
+                    isnothing(Corrρ_B1) || h5write(file_out,joinpath(ens,p0,"B1"),Corrρ_B1)
+                    isnothing(Corrρ_B2) || h5write(file_out,joinpath(ens,p0,"B2"),Corrρ_B2)
                 end
 
                 if plotting
