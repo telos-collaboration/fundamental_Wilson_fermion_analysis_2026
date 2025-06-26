ad0e77e585d51536652a59cee0d65b2e60b7679c
Fabian Zierler <fabian.zierler@a1.net>
2025-06-26 11:53:22 +0100

    new and improved correlator folding

--- scripts/diagram_symmetry.jl
+++ scripts/diagram_symmetry.jl
@@ -4,7 +4,8 @@ using Statistics
 using Plots
 gr(fontfamily="Computer Modern",frame=:box,markeralpha=0.7,titlefontsize=11)
 
-function symmetry_ratio(Corr,T,t_dim)
+function symmetry_ratio(Corr;t_dim)
+    T      = size(Corr,t_dim)
     left   = 2:T÷2
     right  = T÷2+2:T
     Cleft  = selectdim(Corr,t_dim,left)
@@ -12,10 +13,22 @@ function symmetry_ratio(Corr,T,t_dim)
     ratio  = @. Cleft/Cright
     return left, ratio
 end
+function symmetrise_correlator(Corr;t_dim,sign)
+    T      = size(Corr,t_dim)
+    left   = 2:T÷2
+    right  = T÷2+2:T
+    Cleft  = selectdim(Corr,t_dim,left)
+    Cright = selectdim(Corr,t_dim,reverse(right))
+    C0     = selectdim(Corr,t_dim,1:1)
+    Cmid   = selectdim(Corr,t_dim,T÷2+1:T÷2+1)
+    Cleft_sym   = @. (Cleft + sign*Cright)/2 
+    Cright_sym  = @. (sign*Cleft + Cright)/2 
+    C_symmetric = cat(C0,Cleft_sym,Cmid,reverse(Cright_sym,dims=t_dim),dims=t_dim)
+    return C_symmetric
+end
 function symmetry_ratio_avg(Corr;t_dim,conf_dim,avg_dims)
-    T     = size(Corr,t_dim)
     Nconf = size(Corr,conf_dim)
-    t, r  = symmetry_ratio(Corr,T,t_dim)
+    t, r  = symmetry_ratio(Corr;t_dim)
     R     = dropdims(mean(r,dims=avg_dims),dims=avg_dims)
     ΔR    = dropdims(std(r,dims=avg_dims),dims=avg_dims)/sqrt(Nconf)
     t_dim = t_dim - count(d -> isless(d,t_dim), avg_dims)
@@ -33,4 +46,12 @@ Corr2 = read(fid2[(first(keys(fid2)))],"p(0,0,0)/pi/p_diag(0,0,0)/C_re")
 t1, r1, Δr1, t_dim1 = symmetry_ratio_avg(Corr1;t_dim=4,conf_dim=3,avg_dims=(3))
 t2, r2, Δr2, t_dim2 = symmetry_ratio_avg(Corr2;t_dim=3,conf_dim=1,avg_dims=(1,2))
 
-scatter(t2,r2,yerr=Δr2,ylims=(-1.1,1.1))
+
+Corr1_sym = symmetrise_correlator(Corr1;t_dim=4,sign=+1)
+
+Corr1_sym
+
+t1, r1, Δr1, t_dim1 = symmetry_ratio_avg(Corr1_sym;t_dim=4,conf_dim=3,avg_dims=(3))
+scatter(t1,real.(r1[1,1,:]),yerr=real.(Δr1[1,1,:]))
+
+
