ffb7f59b2e298f21bfa24a757d689e624837f986
Fabian Zierler <fabian.zierler@a1.net>
2025-07-14 16:13:25 +0100

    optimise fit intervals

--- main.sh
+++ main.sh
@@ -21,7 +21,7 @@ h5file_scat_fit="data_assets/isospin1_fit_scatter.hdf5"
 
 inputfiles="metadata/input_files.csv"
 infvolfile="metadata/infinite_volume.csv"
-fitparam="metadata/pipi_fitintervals.csv"
+fitparam="metadata/pipi_fitintervals_3x3.csv"
 input_scatter="metadata/scattering_input.csv"
 input_scatter_fit="metadata/fit_scatter_input.csv"
 ensembles_list="metadata/ensembles.csv"
@@ -34,7 +34,7 @@ julia src/scripts_julia/combine_runs.jl --h5file_in $h5file_raw --h5file_out $h5
 julia src/scripts_julia/write_correlation_matrix.jl --h5file_in $h5file_com --h5file_out $h5file_cor --ensembles_list $ensembles_list
 julia src/scripts_julia/write_eigenvalues.jl --h5file_in $h5file_cor --h5file_out $h5file_eig --gevp $gevp --t0 $t0 --deriv $deriv --avg $average_equivalent_momenta --symmetrise $symmetrise
 python3 src/src_py/fitting.py $h5file_eig $h5file_fit $fitparam
-julia src/scripts_julia/write_table_fitresults.jl --h5file $h5file_fit --outfile "$tablepath/fit_results_3x3.csv"
+julia src/scripts_julia/write_table_fitresults.jl --h5file $h5file_fit --outfile "$tablepath/fit_results_3x3_tuned.csv"
 
 julia src/scripts_julia/plot_correlation_matrix.jl --h5file_in $h5file_com --plotpath $plotpath
 julia src/scripts_julia/plot_eigenvalues.jl --h5file_in $h5file_eig --plotpath $plotpath --three_by_three $use3x3
