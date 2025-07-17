147bc960b2977f099de8716c5df3b51a51be3519
Yannick Dengler <yannick.dengler@uni-graz.at>
2025-07-17 19:37:30 +0200

    Which points to scat fit can be choosen, runs

--- src/src_py/fit_scatter.py
+++ src/src_py/fit_scatter.py
@@ -68,19 +68,21 @@ def RES_fit(p3cotPS_ECM, tan_PS, p2):                                   # all pr
 def get_fits(res, res_spl):
     res_tmp={}
     res_spl_tmp={}
-    win_ind = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
-    for key, val in ERE_fit(res["p3cotPS_prime"][win_ind],res["p2star_prime"][win_ind]).items():
+    for P in res["p3cotPS_prime"]:
+        print(P.real)
+    print()
+    for key, val in ERE_fit(res["p3cotPS_prime"],res["p2star_prime"]).items():
         res_tmp[key] = val
-    for key, val in RES_fit(res["p3cotPS_Ecm_prime"][win_ind],res["tan_PS"][win_ind],res["p2star_prime"][win_ind]).items():
+    for key, val in RES_fit(res["p3cotPS_Ecm_prime"],res["tan_PS"],res["p2star_prime"]).items():
         res_tmp[key] = val
         
     for key in res_tmp.keys():
         res_spl_tmp[key] = []
 
     for i in range(len(res_spl["p3cotPS_prime"])):
-        for key, val in ERE_fit(res_spl["p3cotPS_prime"][i][win_ind],res_spl["p2star_prime"][i][win_ind]).items():
+        for key, val in ERE_fit(res_spl["p3cotPS_prime"][i],res_spl["p2star_prime"][i]).items():
             res_spl_tmp[key].append(val)
-        for key, val in RES_fit(res_spl["p3cotPS_Ecm_prime"][i][win_ind],res_spl["tan_PS"][i][win_ind],res_spl["pstar_prime"][i][win_ind]).items():
+        for key, val in RES_fit(res_spl["p3cotPS_Ecm_prime"][i],res_spl["tan_PS"][i],res_spl["pstar_prime"][i]).items():
             res_spl_tmp[key].append(val)
     return res_tmp, res_spl_tmp
 
@@ -97,57 +99,67 @@ def genfromtxt_skip_empty(filename, **kwargs):
         return np.genfromtxt(lines, **kwargs)
 
 def fit_one_phaseshift(h5file_in, h5file_out, input_file, beta, m0):
-    res_calc = {}
-    res_spl_calc = {}
+    res_scat = {}
+    res_spl_scat = {}
 
-    infile = np.transpose(np.genfromtxt_skip_empty(input_file,delimiter=";",skip_header=1,dtype=str))
+    infile = np.transpose(genfromtxt_skip_empty(input_file,delimiter=";",skip_header=1,dtype=str))
 
     with h5py.File(h5file_in,"r") as hfile:
-        for ens in tqdm(hfile):
+        for ens in hfile:
             if str(beta) in ens and str(m0) in ens:
                 for P in hfile[ens]:
                     if P[0] == "p":
-                        # dvec = [int(P[2]),int(P[4]),int(P[6])]
                         for irrep in hfile[ens][P]:
                             for lv in hfile[ens][P][irrep]:
                                 if lv[:2] == "lv":
-                                    # print(ens,P,irrep,lv)
-                                    # print(ens+P+irrep+lv)
-                                    # exit()
-                                    fit = infile[1][infile[0] == ens+P+irrep+lv]
-                                    fit = only(infile[1][infile[0] == ens + P + irrep + lv])
-                                    if fit:
-                                        print(ens+P+irrep+lv)
-                                        if res_calc == {}:
-                                            # num_resample = hfile[ens][P][irrep][lv]["info"]["num_resample"][()]
-                                            for key in hfile[ens][P][irrep][lv]["mean"]:
-                                                res_calc[key] = []
-                                                res_spl_calc[key] = []
-                                        for tmp in hfile[ens][P][irrep][lv]["mean"]:
-                                            res_calc[tmp].append(hfile[ens][P][irrep][lv]["mean"][tmp][()])
-                                            res_spl_calc[tmp].append(hfile[ens][P][irrep][lv]["sample"][tmp][()])
+                                    fit_in = infile[1][infile[0] == ens+P+irrep+lv]
+                                    fit = None
+                                    if len(fit_in) == 0:
+                                        fit = "False"
                                     else:
-                                        print("not\t"+ens+P+irrep+lv)
+                                        fit = fit_in[0]
+                                    if fit == "True":
+                                        pstar_prime = hfile[ens][P][irrep][lv]["mean"]["pstar_prime"][()]
+                                        if 0 < pstar_prime < 3:
+                                            if res_scat == {}:
+                                                for key in hfile[ens][P][irrep][lv]["mean"]:
+                                                    res_scat[key] = []
+                                                    res_spl_scat[key] = []
+                                            for tmp in hfile[ens][P][irrep][lv]["mean"]:
+                                                res_scat[tmp].append(hfile[ens][P][irrep][lv]["mean"][tmp][()])
+                                                res_spl_scat[tmp].append(hfile[ens][P][irrep][lv]["sample"][tmp][()])
+                                        else:
+                                            raise RuntimeError("Energy not in elastic window at: %s"%(ens+P+irrep+lv))
+                                    elif fit == "False":
+                                        pass
+                                    else:
+                                        raise RuntimeError("Wrong assignment in 'fit_scatter_input.csv' at: %s"%(ens+P+irrep+lv))
 
-    for key in res_calc:
-        res_calc[key] = np.asarray(res_calc[key])
-        res_spl_calc[key] = np.transpose(np.asarray(res_spl_calc[key]))
+    
+    if res_scat == {} or len(res_scat["pstar_prime"]) < 3:
+        raise RuntimeError("Less than 3 energy levels selected in 'fit_scatter_input.csv' for beta=%f and m0%f"%(beta, m0))
 
-    res_fit, res_spl_fit = get_fits(res_calc,res_spl_calc)
+    for key in res_scat:
+        res_scat[key] = np.asarray(res_scat[key])
+        res_spl_scat[key] = np.transpose(np.asarray(res_spl_scat[key]))
+
+    
+
+    res_fit, res_spl_fit = get_fits(res_scat,res_spl_scat)
     with h5py.File(h5file_out,"a") as hfile:
         for key, val in res_fit.items():
             hfile.create_dataset("fit_scatter_b%f_m%f/"%(beta,m0)+"mean/"+key, data = val)
         for key, val in res_spl_fit.items():
             hfile.create_dataset("fit_scatter_b%f_m%f/"%(beta,m0)+"sample/"+key, data = val)
-        for key, val in res_calc.items():
+        for key, val in res_scat.items():
             hfile.create_dataset("fit_scatter_b%f_m%f/"%(beta,m0)+"mean/"+key, data = val)
-        for key, val in res_spl_calc.items():
+        for key, val in res_spl_scat.items():
             hfile.create_dataset("fit_scatter_b%f_m%f/"%(beta,m0)+"sample/"+key, data = val)
 
 def fit_all_phase_shifts(h5file_in, h5file_out, input_file):
     fit_one_phaseshift(h5file_in, h5file_out, input_file,6.9,-0.92)
-    # fit_one_phaseshift(h5file_in, h5file_out, input_file,7.05,-0.863)
-    # fit_one_phaseshift(h5file_in, h5file_out, input_file,7.05,-0.867)
+    fit_one_phaseshift(h5file_in, h5file_out, input_file,7.05,-0.863)
+    fit_one_phaseshift(h5file_in, h5file_out, input_file,7.05,-0.867)
 
 
 if __name__ == "__main__":
