9c6cffef8b3c6d583a168cda153463488a4d9aaf
Fabian Zierler <fabian.zierler@a1.net>
2025-09-04 20:17:57 +0100

    avoid code duplication

--- src/src_py/fitting_mesons.py
+++ src/src_py/fitting_mesons.py
@@ -1,62 +1,11 @@
-import gvar as gv
-import corrfitter as cf
-import h5py
-import numpy as np
-import os
 import os.path as op
-import matplotlib.pyplot as plt
+import gvar as gv
+import os
+import h5py
 import csv
 import sys
 from tqdm import tqdm
-
-def get_hdf5_value(hdf5file,key):
-    return hdf5file[key][()]
-
-def make_models(T,tmin,tmax):
-    """  Create corrfitter model for G(t). """
-    return [cf.Corr2(datatag='Gab', tp=T, tmin=tmin, tmax=tmax, a='a', b='a', dE='dE')]
-
-def make_prior(N):
-    prior = gv.BufferDict()
-    # NOTE: use a log-Gaussion distrubtion for forcing positive energies
-    # NOTE: Even with this code they can be recovered by providing loose priors of 0.1(1) for both
-    prior['log(a)']  = gv.log(gv.gvar(N * ['1(1)']))
-    prior['log(dE)'] = gv.log(gv.gvar(N * ['1(1)']))
-    return prior
-
-def first_fit_parameters(fit):
-    p = fit.p
-    E = np.cumsum(p['dE'])
-    a = p['a']
-    chi2 = fit.chi2     
-    dof = fit.dof
-    return E, a, chi2, dof
-
-def print_fit_param(fit):
-    E, a, chi2, dof = first_fit_parameters(fit) 
-    print('{:2}  {:15}  {:15}'.format('E', E[0], E[1]))
-    print('{:2}  {:15}  {:15}'.format('a', a[0], a[1]))
-    print('chi2/dof = ', chi2/dof, '\n')
-
-
-def fit_correlator_without_bootstrap(avg,T,tmin,tmax,Nmax,antisymmetric,plotname="test",plotdir="./tmp/plots/",plotting=False,printing=False):
-    T = - abs(T) if antisymmetric else abs(T) 
-    fitter = cf.CorrFitter(models=make_models(T,tmin,tmax))
-    p0 = None
-    for N in range(1,Nmax+1):
-        prior = make_prior(N)
-        fit = fitter.lsqfit(data=avg, prior=prior, p0=p0)
-        p0 = fit.pmean
-
-    if printing:
-        print_fit_param(fit)
-
-    E, a, chi2, dof = first_fit_parameters(fit) 
-    if plotting:
-        os.makedirs(plotdir+plotname, exist_ok=True)
-        fit.show_plots(view='ratio',save=plotdir+plotname+'/ratio.pdf')
-        fit.show_plots(view='log'  ,save=plotdir+plotname+'/data.pdf')
-    return E, a, chi2, dof
+from corrfitter_utils import get_hdf5_value, fit_correlator_without_bootstrap
 
 def fit_all_files(infile,outfile,parameterfile):
 
