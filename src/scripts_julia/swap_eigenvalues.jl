05184e62741c8b1c69be7573684ffb633aa23464
Fabian Zierler <fabian.zierler@a1.net>
2025-09-11 21:46:14 +0100

    change interface for eigenvalue relabelling

--- src/scripts_julia/swap_eigenvalues.jl
+++ src/scripts_julia/swap_eigenvalues.jl
@@ -50,38 +50,53 @@ function swap_eigvals_cov(old,op1,op2,t)
     @. new[:,op2,r2] = old[:,op1,r2]
     return new
 end
-
-function test_swaps(h5file,csvdat)
-    fid  = h5open(h5file,"cw")
+function _swap_eigenvalues(ev, Δev, ev_cov, op1, op2, t_swap)
+    # load eigenvalues
+    if t_swap > 0
+        ev = swap_eigvals(ev,op1,op2,t_swap)
+        Δev = swap_eigvals(Δev,op1,op2,t_swap)
+        ev_cov = swap_eigvals_cov(ev_cov,op1,op2,t_swap)
+    end
+    if t_swap < 0
+        ev = swap_eigvals_start(ev,op1,op2,abs(t_swap))
+        Δev = swap_eigvals_start(Δev,op1,op2,abs(t_swap))
+        ev_cov = swap_eigvals_cov(ev_cov,op1,op2,abs(t_swap))
+    end
+    return ev, Δev, ev_cov
+end
+function test_swaps(fid,csvdat,r,p,irrep,id)
     data = readdlm(csvdat,',',skipstart=1)
-
+    # load data from hdf5 file
+    ev  = read(fid[r]["$p/$irrep/$id"],"eigvals_3x3")
+    Δev = read(fid[r]["$p/$irrep/$id"],"Delta_eigvals_3x3")
+    ev_cov = read(fid[r]["$p/$irrep/$id"],"cov_eigvals_3x3")    
+    # determine which swaps to perform
     for row in eachrow(data)
-
-        # determine which swaps to perform
-        r, p, irrep, id = row[1:4]
-        op1,op2,t_swap = row[5:7]
-
-        # load eigenvalues
-        T   = read(fid[r],"lattice")[1]
-        ev  = read(fid[r]["$p/$irrep/$id"],"eigvals_3x3")
-        Δev = read(fid[r]["$p/$irrep/$id"],"Delta_eigvals_3x3")
-        ev_cov = read(fid[r]["$p/$irrep/$id"],"cov_eigvals_3x3")
-        
-        if t_swap > 0
-            ev = swap_eigvals(ev,op1,op2,t_swap)
-            Δev = swap_eigvals(Δev,op1,op2,t_swap)
-            ev_cov = swap_eigvals_cov(ev_cov,op1,op2,t_swap)
+        if row[1:4] == [r, p, irrep, id]
+            op1, op2, t_swap = row[5:7]
+            ev, Δev, ev_cov = _swap_eigenvalues(ev, Δev, ev_cov, op1, op2, t_swap)
         end
-        if t_swap < 0
-            ev = swap_eigvals_start(ev,op1,op2,abs(t_swap))
-            Δev = swap_eigvals_start(Δev,op1,op2,abs(t_swap))
-            ev_cov = swap_eigvals_cov(ev_cov,op1,op2,abs(t_swap))
+    end
+    # replace old eigenvalues with the relabelled ones
+    fid[r]["$p/$irrep/$id/eigvals_3x3"][:,:] = ev[:,:]
+    fid[r]["$p/$irrep/$id/Delta_eigvals_3x3"][:,:] = Δev[:,:]
+    fid[r]["$p/$irrep/$id/cov_eigvals_3x3"][:,:,:] = ev_cov[:,:,:]
+end
+function test_swaps(h5file,csvdat;irrep="A1")
+    fid  = h5open(h5file,"cw")
+    runs = keys(fid)
+    # determine which swaps to perform
+    for r in runs
+        momenta = read(fid[r],"p_external")
+        for p in momenta
+            if haskey(fid[r],p) && haskey(fid[r][p],irrep)
+                ids = filter(contains("evp"),keys(fid[r][p][irrep]))
+                for id in ids
+                    id == "gevp" && continue
+                    test_swaps(fid,csvdat,r,p,irrep,id)
+                end
+            end
         end
-
-        # replace old eigenvalues with the relabelled ones
-        fid[r]["$p/$irrep/$id/eigvals_3x3"][:,:] = ev[:,:]
-        fid[r]["$p/$irrep/$id/Delta_eigvals_3x3"][:,:] = Δev[:,:]
-        fid[r]["$p/$irrep/$id/cov_eigvals_3x3"][:,:,:] = ev_cov[:,:,:]
     end
 end
 function parse_commandline()
