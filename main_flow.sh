2cdb7fe0f4504e5c9c1b06fb8765fdf0b8654ce2
Fabian Zierler <fabian.zierler@tum.de>
2026-04-14 12:31:58 +0200

    add parsing of gradient flow to workflow

--- main_flow.sh
+++
@@ -1,5 +0,0 @@
-for filename in raw_data/raw_flow/*/topology/out/out_flow; do 
-    name=$(echo $filename | grep -o "Lt.*FUN")
-    echo $name 
-    python3 src/src_py/package_flows.py --h5_filename data_assets/topology.hdf5 --ensemble $name $filename
-done
