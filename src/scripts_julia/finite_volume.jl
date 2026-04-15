4f146ff37ecccff6288cc48d0cbadfdd53931492
Fabian Zierler <fabian.zierler@tum.de>
2026-04-15 15:33:12 +0200

    add finite volume plots of mesons into the workflow

--- src/scripts_julia/finite_volume.jl
+++ src/scripts_julia/finite_volume.jl
@@ -2,39 +2,63 @@ using Pkg; Pkg.activate("src/src_jl")
 using HDF5
 using Plots
 using LaTeXStrings
+using ArgParse: ArgParseSettings, parse_args, @add_arg_table
 gr(fontfamily="Computer Modern",frame=:box,markeralpha=0.7,size=(300,300),titlefontsize=11)
 
-file = "data_assets/isospin1_fitresults.hdf5"
-h5id = h5open(file)
-dir = "tmp/finite_volume/"
+function finite_volume_plots_appendix(file,dir,table)
+    h5id = h5open(file)
 
-ens = keys(h5id)
-rx  = r"beta(?<b>[0-9]+.[0-9]+)m-(?<m>[0-9]+.[0-9]+)"
-masses = unique(getindex.(match.(rx,ens),"m"))
+    ens = keys(h5id)
+    rx  = r"beta(?<b>[0-9]+.[0-9]+)m-(?<m>[0-9]+.[0-9]+)"
+    masses = unique(getindex.(match.(rx,ens),"m"))
 
-io = open("tmp/fv.csv","w")
-println(io,"ens,β,m,T,L,E_PS,ΔE_PS,E_V,ΔE_V")
+    io = open(table,"w")
+    println(io,"ens,β,m,T,L,E_PS,ΔE_PS,E_V,ΔE_V")
 
-for m in masses
-    ensembles = filter(contains(m),ens)
-    β  = only(String.(unique(getindex.(match.(rx,ensembles),"b"))))
-    T  = [ read(h5id[e],"lattice")[1] for e in ensembles ] 
-    L  = [ read(h5id[e],"lattice")[end] for e in ensembles ] 
-    E  = [ only(read(h5id[e],"p(0,0,0)/pi/E")) for e in ensembles ] 
-    ΔE = [ only(read(h5id[e],"p(0,0,0)/pi/Delta_E")) for e in ensembles ]
-    Eρ = [ only(read(h5id[e],"p(0,0,0)/T1/E")) for e in ensembles ] 
-    ΔEρ= [ only(read(h5id[e],"p(0,0,0)/T1/Delta_E")) for e in ensembles ]
+    for m in masses
+        ensembles = filter(contains(m),ens)
+        β  = only(String.(unique(getindex.(match.(rx,ensembles),"b"))))
+        T  = [ read(h5id[e],"lattice")[1] for e in ensembles ] 
+        L  = [ read(h5id[e],"lattice")[end] for e in ensembles ] 
+        E  = [ only(read(h5id[e],"p(0,0,0)/pi/E")) for e in ensembles ] 
+        ΔE = [ only(read(h5id[e],"p(0,0,0)/pi/Delta_E")) for e in ensembles ]
+        Eρ = [ only(read(h5id[e],"p(0,0,0)/T1/E")) for e in ensembles ] 
+        ΔEρ= [ only(read(h5id[e],"p(0,0,0)/T1/Delta_E")) for e in ensembles ]
 
-    for i in eachindex(L)
-        println(io,"$(ens[i]),$β,$m,$(T[i]),$(L[i]),$(E[i]),$(ΔE[i]),$(Eρ[i]),$(ΔEρ[i])")
+        for i in eachindex(L)
+            println(io,"$(ens[i]),$β,$m,$(T[i]),$(L[i]),$(E[i]),$(ΔE[i]),$(Eρ[i]),$(ΔEρ[i])")
+        end
+
+        # start plotting
+        plt = scatter(inv.(L),E,yerr=ΔE,label=L"\pi")
+        scatter!(inv.(L),Eρ,yerr=ΔEρ,label=L"\rho")
+        plot!(plt,xlabel=L"1/L",ylabel=L"E_{\rm PS}")
+        plot!(plt,xlims=[0,maximum(inv,0.8L)])
+        ispath(dir) || mkpath(dir)
+        savefig(joinpath(dir,"fv_b$(β)_m$m.pdf"))
     end
-
-    # start plotting
-    plt = scatter(inv.(L),E,yerr=ΔE,label=L"\pi")
-    scatter!(inv.(L),Eρ,yerr=ΔEρ,label=L"\rho")
-    plot!(plt,xlabel=L"1/L",ylabel=L"E_{\rm PS}")
-    plot!(plt,xlims=[0,maximum(inv,0.8L)])
-    ispath(dir) || mkpath(dir)
-    savefig(joinpath(dir,"fv_b$(β)_m$m.pdf"))
+    close(io)
 end
-close(io)
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--h5file_in"
+        help = "HDF5 file containing the parsed data"
+        required = true
+        "--plotpath"
+        help = "HDF5 output file containing the correlation matrices"
+        required = true
+        "--table_out"
+        help = "Where to save a CSV file containing the finite volume meson energy levels"
+        required = true
+    end
+    return parse_args(s)
+end
+function main()
+    args = parse_commandline()
+    file = args["h5file_in"]
+    dir = args["plotpath"]
+    table = args["table_out"]
+    finite_volume_plots_appendix(file,dir,table)
+end
+main()
