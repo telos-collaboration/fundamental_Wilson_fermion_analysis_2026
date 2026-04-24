dcb1d1afd0da387ca13e8959b3846d53e9c5a4fc
Fabian Zierler <fabian.zierler@a1.net>
2026-04-24 20:17:31 +0200

    integrate remaining tables into the analysis

--- src/scripts_julia/phase_shift_table.jl
+++ src/scripts_julia/phase_shift_table.jl
@@ -3,6 +3,7 @@ using HDF5
 using Plots
 using Statistics
 using SpecialFunctions
+using ArgParse: ArgParseSettings, parse_args, @add_arg_table
 
 function uncertainty_from_samples(samples)
     # Yannick encodes missing/failed determinations with 0
@@ -23,46 +24,64 @@ function uncertainty_from_samples(samples)
     shift = Int(round(length(s)*erf(1/sqrt(2))/2))
     x_upper = s[ind+shift]
     x_lower = s[ind-shift]
-    # If the upper and lower bound are very asymmetric, we will discard
-    # them, since the underlying distribution appears to be non-Gaussian
+
     Δx_upper = abs(x - x_upper)
     Δx_lower = abs(x - x_lower)
-    if (1/2) < Δx_upper/Δx_lower < 2 
-        Δx = max(Δx_upper, Δx_lower)
-        return x, Δx
-    end
-    return NaN, NaN
+    Δx = max(Δx_upper, Δx_lower)
+    return x, Δx
 end
 
-file = "data_assets/isospin1_fit_scatter.hdf5"
-h5id = h5open(file)
-ensembles = keys(h5id)
-
-@show file
-
-is_heavy(ensemble) = endswith(ensemble,"beta6.9m-0.92")
-is_medium(ensemble) = endswith(ensemble,"beta7.05m-0.863")
-is_light(ensemble) = endswith(ensemble,"beta7.05m-0.867")
-
-for ens in ensembles
-    if is_heavy(ens)
-        Λ = read(h5id[ens],"lattice")
-        Nt, Ns = Λ[1], Λ[2]
-        momenta = filter(startswith("p"),keys(h5id[ens])) 
-        for mom in momenta
-            irreps = filter( !isequal("pi"), keys(h5id[ens][mom]))
-            for ir in irreps
-                # obtain the number of energy levels from the available data sets
-                h5channel = h5id[ens][mom][ir]
-                levels = filter(startswith("lv"),keys(h5channel))
-                for l in levels
-                    # We need to work out the uncertainties 
-                    s_sample = read(h5channel[l]["sample"],"s")
-                    δ_sample = read(h5channel[l]["sample"],"PS")
-                    δ, Δδ = uncertainty_from_samples(real.(δ_sample))
-                    s, Δs = uncertainty_from_samples(real.(s_sample))
+function phase_shift_table_csv(infile,outfile,pattern)
+    h5id = h5open(infile)
+    ensembles = keys(h5id)
+    io = open(outfile,"w")
+    for ens in ensembles
+        if endswith(ens,pattern)
+            Λ = read(h5id[ens],"lattice")
+            Nt, Ns = Λ[1], Λ[2]
+            momenta = filter(startswith("p"),keys(h5id[ens])) 
+            for mom in momenta
+                irreps = filter( !isequal("pi"), keys(h5id[ens][mom]))
+                for ir in irreps
+                    # obtain the number of energy levels from the available data sets
+                    h5channel = h5id[ens][mom][ir]
+                    # read fitted energy levels
+                    E = read(h5channel,"E")
+                    ΔE = read(h5channel,"Delta_E")
+                    levels = filter(startswith("lv"),keys(h5channel))
+                    for (i,l) in enumerate(levels)
+                        # We need to work out the uncertainties 
+                        s_sample = read(h5channel[l]["sample"],"s")
+                        δ_sample = read(h5channel[l]["sample"],"PS")
+                        δ, Δδ = uncertainty_from_samples(real.(δ_sample))
+                        rs, Δrs = uncertainty_from_samples(real.(sqrt.(s_sample)))
+                        println(io,"$pattern;$Nt;$Ns;$mom;$ir;$i;$(E[i]);$(ΔE[i]);$(rs);$(Δrs);$δ;$Δδ")
+                    end
                 end
             end
         end
     end
+    close(io)
 end
+
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--h5file_in"
+        help = "HDF5 file containing the parsed data"
+        required = true
+        "--outfile"
+        help = "CSV output containing the list of all parsed runs"
+        required = true
+        "--pattern"
+        help = "label of the ensemble in the form 'beta{coupling}m{mass}' "
+        default = nothing
+    end
+    return parse_args(s)
+end
+
+function main()
+    args = parse_commandline()
+    phase_shift_table_csv(args["h5file_in"],args["outfile"],args["pattern"])
+end 
+main()
