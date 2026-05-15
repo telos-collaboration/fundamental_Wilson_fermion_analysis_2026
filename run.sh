4231fdbe79346ebfd02d59c3ddf3bf7a40299352
Fabian Zierler <fabian.zierler@a1.net>
2026-05-15 21:15:30 +0200

    unify libs/ directory

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 cd scattering
-snakemake --use-conda --cores 8
-#cd ../spectrum
-#snakemake --use-conda --cores 8
-#cd -
+snakemake --use-conda --cores 8 --forceall --rerun-incomplete
+cd -
+cd spectrum
+snakemake --use-conda --cores 8 --forceall --rerun-incomplete
+cd -
